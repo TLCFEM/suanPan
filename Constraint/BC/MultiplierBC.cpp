4d6e81481213b0a6addd42b07961149752b31344
tlc@workhorse <tlcfem@gmail.com>
2021-04-17 03:14:43 +1200

    MINOR UPDATE

--- Constraint/BC/MultiplierBC.cpp
+++ Constraint/BC/MultiplierBC.cpp
@@ -29,7 +29,9 @@
 int MultiplierBC::process(const shared_ptr<DomainBase>& D) {
 	if(auto& t_step = *D->get_current_step(); typeid(Frequency) == typeid(t_step)) return PenaltyBC::process(D);
 
-	auto& t_matrix = D->get_factory()->get_stiffness();
+	auto& t_stiff = D->get_factory()->get_stiffness();
+	// auto& t_damping = D->get_factory()->get_damping();
+	// auto& t_mass = D->get_factory()->get_mass();
 
 	for(const auto& I : node_encoding)
 		if(auto& t_node = D->get<Node>(I); nullptr != t_node && t_node->is_active()) {
@@ -38,7 +40,9 @@ int MultiplierBC::process(const shared_ptr<DomainBase>& D) {
 				if(J <= t_dof.n_elem) {
 					const auto& t_idx = t_dof(J - 1);
 					D->insert_restrained_dof(t_idx);
-					t_matrix->unify(t_idx);
+					t_stiff->unify(t_idx);
+					// if(nullptr != t_damping) t_damping->unify(t_idx);
+					// if(nullptr != t_mass) t_mass->unify(t_idx);
 				}
 		}
 
