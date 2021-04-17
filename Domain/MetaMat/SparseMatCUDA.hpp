12d989c9293f1c4522cbe8612d54b8824c4fe6eb
tlc@workhorse <tlcfem@gmail.com>
2021-04-17 22:29:26 +1200

    UPDATE CUDA SPARSE SOLVER

--- Domain/MetaMat/SparseMatCUDA.hpp
+++ Domain/MetaMat/SparseMatCUDA.hpp
@@ -26,6 +26,7 @@
  * @{
  */
 
+// ReSharper disable CppCStyleCast
 #ifndef SPARSEMATCUDA_HPP
 #define SPARSEMATCUDA_HPP
 
@@ -35,68 +36,75 @@
 #include <cusparse.h>
 
 template<typename T> class SparseMatCUDA final : public SparseMat<T> {
-	static void cu_destory(cusolverSpHandle_t);
+	cusolverSpHandle_t handle = nullptr;
+	cusparseMatDescr_t descrA = nullptr;
+
+	void cu_create();
+	void cu_destory() const;
 public:
 	using SparseMat<T>::tolerance;
 	using SparseMat<T>::triplet_mat;
 
 	SparseMatCUDA();
 	SparseMatCUDA(uword, uword, uword = 0);
+	~SparseMatCUDA() override;
 
 	unique_ptr<MetaMat<T>> make_copy() override;
 
 	int solve(Mat<T>&, const Mat<T>&) override;
 };
 
-template<typename T> void SparseMatCUDA<T>::cu_destory(const cusolverSpHandle_t handle) { if(const auto solver_info = cusolverSpDestroy(handle); CUSOLVER_STATUS_SUCCESS != solver_info) suanpan_error("error code %u returned by CUDA.\n", static_cast<unsigned>(solver_info)); }
+template<typename T> void SparseMatCUDA<T>::cu_create() {
+	cusolverSpCreate(&handle);
+	cusparseCreateMatDescr(&descrA);
+	cusparseSetMatIndexBase(descrA, CUSPARSE_INDEX_BASE_ZERO);
+	cusparseSetMatType(descrA, CUSPARSE_MATRIX_TYPE_GENERAL);
+}
+
+template<typename T> void SparseMatCUDA<T>::cu_destory() const {
+	cusparseDestroyMatDescr(descrA);
+	cusolverSpDestroy(handle);
+}
 
 template<typename T> SparseMatCUDA<T>::SparseMatCUDA()
-	: SparseMat<T>() {}
+	: SparseMat<T>() { cu_create(); }
 
 template<typename T> SparseMatCUDA<T>::SparseMatCUDA(const uword in_row, const uword in_col, const uword in_elem)
-	: SparseMat<T>(in_row, in_col, in_elem) {}
+	: SparseMat<T>(in_row, in_col, in_elem) { cu_create(); }
+
+template<typename T> SparseMatCUDA<T>::~SparseMatCUDA() { cu_destory(); }
 
 template<typename T> unique_ptr<MetaMat<T>> SparseMatCUDA<T>::make_copy() { return make_unique<SparseMatCUDA<T>>(*this); }
 
 template<typename T> int SparseMatCUDA<T>::solve(Mat<T>& out_mat, const Mat<T>& in_mat) {
-	cusolverSpHandle_t handle = nullptr;
-	auto solver_info = cusolverSpCreate(&handle);
-
-	if(CUSOLVER_STATUS_SUCCESS != solver_info) {
-		suanpan_error("error code %u returned during initialisation.\n", static_cast<unsigned>(solver_info));
-		cu_destory(handle);
-		return SUANPAN_FAIL;
-	}
-
-	cusparseMatDescr_t descrA = nullptr;
-	if(const auto sparse_info = cusparseCreateMatDescr(&descrA); CUSPARSE_STATUS_SUCCESS != sparse_info) {
-		suanpan_error("error code %u returned during initialisation.\n", static_cast<unsigned>(sparse_info));
-		cu_destory(handle);
-		return SUANPAN_FAIL;
-	}
-
 	csr_form<T, int> csr_mat(triplet_mat);
 
 	out_mat.set_size(size(in_mat));
 
 	int singularity;
 
-	solver_info = cusolverSpDcsrlsvluHost(handle, csr_mat.n_rows, csr_mat.c_size, descrA, csr_mat.val_idx, csr_mat.row_ptr, csr_mat.col_idx, in_mat.memptr(), tolerance, 2, out_mat.memptr(), &singularity);
-
-	if(-1 == singularity) singularity = SUANPAN_SUCCESS;
-	else {
-		suanpan_error("the matrix is singular.\n");
-		singularity = SUANPAN_FAIL;
+	if(cusolverStatus_t solver_info; std::is_same<T, float>::value) {
+		using E = float;
+		for(auto I = 0; I < in_mat.n_cols; ++I) {
+			solver_info = cusolverSpScsrlsvluHost(handle, csr_mat.n_rows, csr_mat.c_size, descrA, (E*)csr_mat.val_idx, csr_mat.row_ptr, csr_mat.col_idx, (E*)in_mat.colptr(I), tolerance, 2, (E*)out_mat.colptr(I), &singularity);
+			if(CUSOLVER_STATUS_SUCCESS != solver_info) {
+				suanpan_error("error code %u returned during CUDA solving.\n", static_cast<unsigned>(solver_info));
+				return SUANPAN_FAIL;
+			}
+		}
+	}
+	else if(std::is_same<T, double>::value) {
+		using E = double;
+		for(auto I = 0; I < in_mat.n_cols; ++I) {
+			solver_info = cusolverSpDcsrlsvluHost(handle, csr_mat.n_rows, csr_mat.c_size, descrA, (E*)csr_mat.val_idx, csr_mat.row_ptr, csr_mat.col_idx, (E*)in_mat.colptr(I), tolerance, 2, (E*)out_mat.colptr(I), &singularity);
+			if(CUSOLVER_STATUS_SUCCESS != solver_info) {
+				suanpan_error("error code %u returned during CUDA solving.\n", static_cast<unsigned>(solver_info));
+				return SUANPAN_FAIL;
+			}
+		}
 	}
 
-	if(CUSOLVER_STATUS_SUCCESS != solver_info) {
-		suanpan_error("error code %u returned during CUDA solving.\n", static_cast<unsigned>(solver_info));
-		singularity = SUANPAN_FAIL;
-	}
-
-	cu_destory(handle);
-
-	return singularity;
+	return SUANPAN_SUCCESS;
 }
 
 #endif
