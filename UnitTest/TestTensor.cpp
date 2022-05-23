8b7f87e2b82b7a905407eff97a7fd9740fffc55e
tlc@wonderland <tlcfem@gmail.com>
2022-05-23 22:26:00 +0200

    Add coverage

--- UnitTest/TestTensor.cpp
+++ UnitTest/TestTensor.cpp
@@ -1,24 +1,49 @@
 #include <Toolbox/tensorToolbox.h>
 #include "CatchHeader.h"
 
-TEST_CASE("Invariant", "[Utility.Tensor]") {
+using namespace tensor;
+
+TEST_CASE("Fixed Stress Invariant", "[Utility.Tensor]") {
     const vec A{50, -20, 10, 30, -10, 20};
 
     REQUIRE(Approx(40) == tensor::stress::invariant1(A));
     REQUIRE(Approx(2100) == tensor::stress::invariant2(A));
     REQUIRE(Approx(-28000) == tensor::stress::invariant3(A));
 
-    const auto B = eig_sym(tensor::stress::to_tensor(A)).eval();
+    const auto B = eig_sym(stress::to_tensor(A)).eval();
 
     REQUIRE(Approx(40) == tensor::stress::invariant1(B));
     REQUIRE(Approx(2100) == tensor::stress::invariant2(B));
     REQUIRE(Approx(-28000) == tensor::stress::invariant3(B));
 }
 
+TEST_CASE("Random Stress/Strain Invariant", "[Utility.Tensor]") {
+    for(auto I = 0; I < 10000; ++I) {
+        const vec A(6, fill::randn);
+
+        mat M = stress::to_tensor(A), E;
+        vec B;
+
+        eig_sym(B, E, M, "std");
+
+        REQUIRE(std::fabs(stress::invariant1(A) - stress::invariant1(B)) <= 1E-13);
+        REQUIRE(std::fabs(stress::invariant2(A) - stress::invariant2(B)) <= 1E-13);
+        REQUIRE(std::fabs(stress::invariant3(A) - stress::invariant3(B)) <= 1E-13);
+
+        M = strain::to_tensor(A);
+
+        eig_sym(B, E, M, "std");
+
+        REQUIRE(std::fabs(strain::invariant1(A) - strain::invariant1(B)) <= 1E-13);
+        REQUIRE(std::fabs(strain::invariant2(A) - strain::invariant2(B)) <= 1E-13);
+        REQUIRE(std::fabs(strain::invariant3(A) - strain::invariant3(B)) <= 1E-13);
+    }
+}
+
 TEST_CASE("Lode Angle", "[Utility.Tensor]") {
     const vec A{30, -20, 10, 30, -10, 20};
 
-    const auto B = tensor::stress::to_tensor(tensor::dev(A));
+    const auto B = stress::to_tensor(dev(A));
 
     REQUIRE(tensor::stress::lode(eig_sym(B / sqrt(accu(square(B)))).eval()) == Approx(tensor::stress::lode(A)));
 }
@@ -39,4 +64,12 @@ TEST_CASE("Rotation of Strain", "[Utility.Tensor]") {
     const auto strain = transform::strain::rotate({.01, -.01, 0.}, .25 * datum::pi);
 
     REQUIRE(Approx(.02) == tensor::strain::norm(strain));
+}
+
+TEST_CASE("Basic Quantites", "[Utility.Tensor]") {
+    const vec A = dev(dev(unit_symmetric_tensor4())).diag();
+    const auto B = stress::norm(A), C = stress::norm(vec(A)), D = strain::norm(A), E = strain::norm(vec(A));
+
+    REQUIRE(Approx(B) == C);
+    REQUIRE(Approx(D) == D);
 }
