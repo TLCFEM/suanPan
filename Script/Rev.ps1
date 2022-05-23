7ae3b25d7144598701feed0c5337611b155b8737
tlc@void <tlcfem@gmail.com>
2022-05-23 10:50:21 +0200

    Update revision setting

--- Script/Rev.ps1
+++ Script/Rev.ps1
@@ -1,3 +1,12 @@
+try
+{
+    git | Out-Null
+}
+catch [System.Management.Automation.CommandNotFoundException]
+{
+    exit
+}
+
 Set-Variable -Name "git_rev" -Value (git rev-parse --short=8 HEAD)
 
 Write-Output "constexpr auto SUANPAN_REVISION = ""$git_rev"";" | Out-File -Encoding utf8 Toolbox/revision.h
