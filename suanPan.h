10902f63f7221b0f58619aa1b7b655202dfcc9f7
tlc@horse <tlcfem@gmail.com>
2022-06-11 08:13:53 +0200

    Correct return type

--- suanPan.h
+++ suanPan.h
@@ -232,7 +232,9 @@ template<class T> concept sp_d = std::is_floating_point_v<T>;
 template<class T> concept sp_i = std::is_integral_v<T>;
 
 namespace suanpan {
-    template<class IN, class FN> void for_all(IN& from, FN&& func) { return suanpan_for_each(from.begin(), from.end(), std::forward<FN>(func)); }
+    template<class IN, class FN> void for_all(IN& from, FN&& func) {
+        suanpan_for_each(from.begin(), from.end(), std::forward<FN>(func));
+    }
 }
 
 #ifdef SUANPAN_CLANG
