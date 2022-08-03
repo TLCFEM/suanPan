70b419084bca833230c221809a519cb8375d15a0
tlc@horse <tlcfem@gmail.com>
2022-08-03 18:45:04 +0200

    Add random sleep

--- Script/Rev.ps1
+++ Script/Rev.ps1
@@ -6,6 +6,10 @@ catch [System.Management.Automation.CommandNotFoundException] {
 }
 
 if (Test-Path -Path '.git') {
+    $rnd = Get-Random -Minimum 1 -Maximum 500
+
+    Start-Sleep -Milliseconds $rnd
+
     Set-Variable -Name "git_rev" -Value (git rev-parse --short=8 HEAD)
 
     Write-Output "constexpr auto SUANPAN_REVISION = ""$git_rev"";" | Out-File -Encoding utf8 Toolbox/revision.h
