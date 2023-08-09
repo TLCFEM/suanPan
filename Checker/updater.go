e8dfaa5772956f4c7b5ffb85f5ead086cf958e05
Theodore Chang <tlcfem@gmail.com>
2023-08-09 02:31:18 +0200

    Minor update

--- Checker/updater.go
+++ Checker/updater.go
@@ -9,14 +9,13 @@ import (
 	"regexp"
 	"runtime"
 	"strconv"
+	"strings"
 	"time"
 )
 
 const URL = "https://github.com/TLCFEM/suanPan/releases"
 
 func main() {
-	fmt.Printf("Checking new version, delete/rename file updater/updater.exe or execute with '-nu' flag if not wanted.\n")
-
 	client := &http.Client{
 		Timeout: 2 * time.Second,
 	}
@@ -43,6 +42,8 @@ func main() {
 	newVersion := string(regex.Find(html))
 
 	if len(os.Args) > 1 {
+		fmt.Printf("Checking new version, delete/rename file updater/updater.exe or execute with '-nu' flag if not wanted.\n")
+
 		number := regex.FindStringSubmatch(newVersion)
 
 		newMajor, _ := strconv.Atoi(number[1])
@@ -58,9 +59,11 @@ func main() {
 		if 100*newMajor+10*newMinor+newPatch <= currentVersion {
 			return
 		}
-
-		_ = downloadLatestVersion(newVersion)
+	} else {
+		fmt.Printf("Downloading the latest version.\n")
 	}
+
+	_ = downloadLatestVersion(newVersion)
 
 	return
 }
@@ -90,6 +93,8 @@ func downloadLatestVersion(versionString string) error {
 	fmt.Printf("      If performance degradation is spotted on your platform, consider switch to `openblas` version.\n")
 	fmt.Printf("  `vtk` uses VTK for visualisation.\n")
 	fmt.Printf("      Visualisation may be useful when it comes to post-processing, but it requires OpenGL support. Please make sure the corresponding packages are installed.\n")
+	fmt.Printf("  `no-avx` disables AVX.\n")
+	fmt.Printf("      For CPUs that do not support AVX, please use this version.\n")
 	fmt.Printf("\nDownload the new version:\n")
 
 	regex, _ := regexp.Compile(`suanPan-v(\d)\.(\d)\.?(\d?)`)
@@ -207,7 +212,14 @@ func downloadLatestVersion(versionString string) error {
 
 	fmt.Printf("Downloaded %s\n", absPath)
 
-	fmt.Printf("You can manually extract the archive to overwrite the existing folder.")
+	isArchive := false
+	isArchive = isArchive || strings.HasSuffix(absPath, "zip")
+	isArchive = isArchive || strings.HasSuffix(absPath, "tar.gz")
+	isArchive = isArchive || strings.HasSuffix(absPath, "7z")
+
+	if isArchive {
+		fmt.Printf("You can manually extract the archive to overwrite the existing folder.")
+	}
 
 	return nil
 }
