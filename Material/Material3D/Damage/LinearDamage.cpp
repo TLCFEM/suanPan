2dadb0e5643fae78cf0c8574d801984c8dc2e6d8
Theodore Chang <tlcfem@gmail.com>
2023-09-03 13:04:28 +0200

    Reformat

--- Material/Material3D/Damage/LinearDamage.cpp
+++ Material/Material3D/Damage/LinearDamage.cpp
@@ -21,50 +21,50 @@
 const double LinearDamage::root_two_third = sqrt(2. / 3.);
 
 LinearDamage::LinearDamage(const unsigned T, const unsigned MT, const double SE, const double EE, const double ED)
-	: IsotropicDamage(T, MT)
-	, s_strain(SE)
-	, e_strain(EE)
-	, e_damage(ED) {}
+    : IsotropicDamage(T, MT)
+    , s_strain(SE)
+    , e_strain(EE)
+    , e_damage(ED) {}
 
 int LinearDamage::initialize(const shared_ptr<DomainBase>& D) {
-	initialize_history(1);
+    initialize_history(1);
 
-	return IsotropicDamage::initialize(D);
+    return IsotropicDamage::initialize(D);
 }
 
 void LinearDamage::compute_damage() {
-	const auto dev_strain = tensor::dev(trial_strain);
-	const auto eqv_strain = root_two_third * tensor::strain::norm(dev_strain);
+    const auto dev_strain = tensor::dev(trial_strain);
+    const auto eqv_strain = root_two_third * tensor::strain::norm(dev_strain);
 
-	const auto compute_damage_factor = [&](const double t_strain) {
-		if(t_strain <= s_strain) return vec{1., 0.};
+    const auto compute_damage_factor = [&](const double t_strain) {
+        if(t_strain <= s_strain) return vec{1., 0.};
 
-		if(t_strain >= e_strain) return vec{e_damage, 0.};
+        if(t_strain >= e_strain) return vec{e_damage, 0.};
 
-		return vec{1. + slope * (t_strain - s_strain), slope};
-	};
+        return vec{1. + slope * (t_strain - s_strain), slope};
+    };
 
-	trial_history = current_history;
-	if(eqv_strain > trial_history(0)) {
-		trial_history(0) = eqv_strain;
-		const auto damage = compute_damage_factor(trial_history(0));
-		const auto& d = damage(0);
-		const auto& dd = damage(1);
+    trial_history = current_history;
+    if(eqv_strain > trial_history(0)) {
+        trial_history(0) = eqv_strain;
+        const auto damage = compute_damage_factor(trial_history(0));
+        const auto& d = damage(0);
+        const auto& dd = damage(1);
 
-		trial_stiffness *= d;
+        trial_stiffness *= d;
 
-		trial_stiffness += trial_stress * (tensor::strain::norm_weight % dev_strain).t() * dd / eqv_strain / 1.5;
+        trial_stiffness += trial_stress * (tensor::strain::norm_weight % dev_strain).t() * dd / eqv_strain / 1.5;
 
-		trial_stress *= d;
-	}
-	else {
-		const auto damage = compute_damage_factor(trial_history(0));
-		const auto& d = damage(0);
+        trial_stress *= d;
+    }
+    else {
+        const auto damage = compute_damage_factor(trial_history(0));
+        const auto& d = damage(0);
 
-		trial_stiffness *= d;
+        trial_stiffness *= d;
 
-		trial_stress *= d;
-	}
+        trial_stress *= d;
+    }
 }
 
 unique_ptr<Material> LinearDamage::get_copy() { return make_unique<LinearDamage>(*this); }
