4f9f5b66fd9f19ae3c4be1b52580c53de0d97b92
Theodore Chang <tlcfem@gmail.com>
2023-09-06 15:11:29 +0200

    Parallel build script

--- Script/CompileAll.ps1
+++ Script/CompileAll.ps1
@@ -1,11 +1,28 @@
-if (Test-Path -Path "CompileAll.ps1") {
-    Set-Location -Path ..
+# This script collects all cmake-build* folders and compile them in parallel.
+# It is mainly used to test different compilers and configurations.
+# The cmake-build* naming convention is used by CLion.
+# CLion also enables auto-reloading of CMakeLists.txt files, which means the cmake-build* folders are always up-to-date.
+# One can then invoke this script such that
+# ```ps
+# .\Script\CompileAll.ps1
+# ```
+
+if (Test-Path -Path "CompileAll.ps1") { Set-Location -Path .. }
+
+$folders = Get-ChildItem -Directory -Name -Path . | Where-Object { $_ -like "cmake-build*" } | ForEach-Object { Convert-Path $_ }
+$folder_count = $folders.length
+$per_core = [int][math]::Ceiling($env:NUMBER_OF_PROCESSORS / ($folder_count - 1) - 1)
+
+$script = {
+    param ( 
+        [string]$folder,
+        [int]$jobs
+    )
+    Set-Location $folder
+    Write-Host "Compiling $folder with $jobs jobs."
+    cmake --build . --parallel $jobs
 }
 
-$folders = Get-ChildItem -Directory -Name -Path . | Where-Object { $_ -like "cmake-build*" }
-foreach ($folder in $folders) {
-    Write-Output "Compiling $folder"
-    Set-Location -Path $folder
-    cmake --build .
-    Set-Location -Path ..
-}
+foreach ($folder in $folders) { Start-Job -ScriptBlock $script -ArgumentList $folder, $per_core }
+
+Wait-Job -Job (Get-Job) | Receive-Job
