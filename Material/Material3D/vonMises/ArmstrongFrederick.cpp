466f2fc34f2a853f9478123e9e5370fbb94c1f35
Theodore Chang <tlcfem@gmail.com>
2024-10-13 03:39:46 +0200

    Relax convergence criterion

--- Material/Material3D/vonMises/ArmstrongFrederick.cpp
+++ Material/Material3D/vonMises/ArmstrongFrederick.cpp
@@ -95,7 +95,7 @@ int ArmstrongFrederick::update_trial_status(const vec& t_strain) {
         const auto error = fabs(incre);
         if(1u == counter) ref_error = error;
         suanpan_debug("Local iteration error: {:.5E}.\n", error);
-        if(error < tolerance * ref_error || (fabs(yield_func) < tolerance && counter > 5u)) break;
+        if(error < tolerance * ref_error || ((fabs(yield_func) < tolerance || error < tolerance) && counter > 5u)) break;
 
         gamma -= incre;
         p -= incre;
