a891abd7481239c5dfb018d1f95b448fd11a0c47
Theodore Chang <tlcfem@gmail.com>
2024-10-27 03:54:30 +0100

    Update convergence criterion

--- Material/Material1D/vonMises/ArmstrongFrederick1D.cpp
+++ Material/Material1D/vonMises/ArmstrongFrederick1D.cpp
@@ -91,7 +91,7 @@ int ArmstrongFrederick1D::update_trial_status(const vec& t_strain) {
         const auto error = fabs(incre);
         if(1u == counter) ref_error = error;
         suanpan_debug("Local iteration error: {:.5E}.\n", error);
-        if(error < tolerance * ref_error || ((fabs(yield_func) < tolerance || error < datum::eps) && counter > 5u)) break;
+        if(error < tolerance * ref_error || ((error < tolerance || fabs(yield_func) < tolerance) && counter > 5u)) break;
 
         gamma -= incre;
         q -= incre;
--- Material/Material1D/vonMises/NonlinearGurson1D.cpp
+++ Material/Material1D/vonMises/NonlinearGurson1D.cpp
@@ -100,7 +100,7 @@ int NonlinearGurson1D::update_trial_status(const vec& t_strain) {
         const auto error = inf_norm(incre);
         if(1u == counter) ref_error = error;
         suanpan_debug("Local iteration error: {:.5E}.\n", error);
-        if(error < tolerance * ref_error || (inf_norm(residual) < tolerance && counter > 5u)) break;
+        if(error < tolerance * ref_error || ((error < tolerance || inf_norm(residual) < tolerance) && counter > 5u)) break;
 
         gamma -= incre(0);
         pe -= incre(1);
