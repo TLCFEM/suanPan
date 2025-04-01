b8154bb8311b7fc29adaa18078db284a3bc22075
Theodore Chang <tlcfem@gmail.com>
2025-04-01 03:46:42 +0200

    Add support for SparseMatClusterLIS in the Factory and include necessary headers

--- Domain/Factory.hpp
+++ Domain/Factory.hpp
@@ -1480,11 +1480,13 @@ template<sp_d T> unique_ptr<MetaMat<T>> Factory<T>::get_basic_container() {
 #ifdef SUANPAN_MKL
         if(contain_solver_type(SolverType::PARDISO)) return std::make_unique<SparseMatClusterPARDISO<T>>(n_size, n_size, n_elem);
 #endif
+        if(contain_solver_type(SolverType::LIS)) return std::make_unique<SparseMatClusterLIS<T>>(n_size, n_size, n_elem);
         return std::make_unique<SparseMatClusterMUMPS<T>>(n_size, n_size, n_elem);
     case StorageScheme::SPARSESYMM:
 #ifdef SUANPAN_MKL
         if(contain_solver_type(SolverType::PARDISO)) return std::make_unique<SparseSymmMatClusterPARDISO<T>>(n_size, n_size, n_elem);
 #endif
+        if(contain_solver_type(SolverType::LIS)) return std::make_unique<SparseMatClusterLIS<T>>(n_size, n_size, n_elem);
         return std::make_unique<SparseSymmMatClusterMUMPS<T>>(n_size, n_size, n_elem);
     default:
         throw invalid_argument("need a proper storage scheme");
