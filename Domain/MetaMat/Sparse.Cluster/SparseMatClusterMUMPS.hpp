103e3d1661a7f8bd17c85a073460959334f9c47b
Theodore Chang <tlcfem@gmail.com>
2025-04-10 20:21:31 +0200

    Fix sign_det method to correctly evaluate determinant as a real number

--- Domain/MetaMat/Sparse.Cluster/SparseMatClusterMUMPS.hpp
+++ Domain/MetaMat/Sparse.Cluster/SparseMatClusterMUMPS.hpp
@@ -50,7 +50,7 @@ public:
 
     unique_ptr<MetaMat<T>> make_copy() override { return std::make_unique<SparseMatBaseClusterMUMPS>(*this); }
 
-    [[nodiscard]] int sign_det() const override { return solver.det() > T(0) ? 1 : -1; }
+    [[nodiscard]] int sign_det() const override { return solver.det().real() > T(0) ? 1 : -1; }
 };
 
 #pragma GCC diagnostic push
