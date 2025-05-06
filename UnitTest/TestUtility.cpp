3ef320b633266d7ed09825311b0c8b65d571b694
Theodore Chang <tlcfem@gmail.com>
2025-05-06 22:48:37 +0200

    Add unit tests for sketching matrix utility functions

--- UnitTest/TestUtility.cpp
+++ UnitTest/TestUtility.cpp
@@ -1,5 +1,6 @@
 #include "CatchHeader.h"
 
+#include <Toolbox/sketching.hpp>
 #include <Toolbox/utility.h>
 #include <thread>
 
@@ -41,4 +42,18 @@ TEST_CASE("Color Print", "[Utility.Print]") {
     suanpan_error("TEST.\n");
     suanpan_fatal("TEST.\n");
     suanpan_info("TEST.\n", vec{1, 2, 3});
+}
+
+TEST_CASE("Sketching Matrix", "[Utility.Matrix]") {
+    mat A(1000, 200, fill::randu);
+    const mat ATA = A.t() * A;
+    const auto norm = norm2est(ATA);
+
+    auto B = frequent_row_directions(A, 71);
+    suanpan_info("{:.5e}\n", norm2est(ATA - B.t() * B) / norm);
+
+    inplace_trans(A);
+
+    B = frequent_col_directions(A, 71);
+    suanpan_info("{:.5e}\n", norm2est(ATA - B * B.t()) / norm);
 }
