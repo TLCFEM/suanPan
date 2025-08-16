00331f96b577bfb1561a5bbf4ffe0e50989497e8
Theodore Chang <tlcfem@gmail.com>
2025-08-16 18:42:57 +0200

    Add extra include for LAPACK subroutines and adjust namespace in armadillo files

--- Include/armadillo/ext_def_lapack.hpp
+++ Include/armadillo/ext_def_lapack.hpp
@@ -1,3 +1,4 @@
+namespace arma {
 #ifdef ARMA_BLAS_CAPITALS
 
 #define arma_sgbmv SGBMV
@@ -84,69 +85,70 @@
 
 #endif
 
-extern "C" {
-void arma_fortran(arma_sgbmv)(const char* TRANS, const blas_int* M, const blas_int* N, const blas_int* KL, const blas_int* KU, const float* ALPHA, const float* A, const blas_int* LDA, const float* X, const blas_int* INCX, const float* BETA, float* Y, const blas_int* INCY);
+    extern "C" {
+    void arma_fortran(arma_sgbmv)(const char* TRANS, const blas_int* M, const blas_int* N, const blas_int* KL, const blas_int* KU, const float* ALPHA, const float* A, const blas_int* LDA, const float* X, const blas_int* INCX, const float* BETA, float* Y, const blas_int* INCY);
 
-void arma_fortran(arma_dgbmv)(const char* TRANS, const blas_int* M, const blas_int* N, const blas_int* KL, const blas_int* KU, const double* ALPHA, const double* A, const blas_int* LDA, const double* X, const blas_int* INCX, const double* BETA, double* Y, const blas_int* INCY);
+    void arma_fortran(arma_dgbmv)(const char* TRANS, const blas_int* M, const blas_int* N, const blas_int* KL, const blas_int* KU, const double* ALPHA, const double* A, const blas_int* LDA, const double* X, const blas_int* INCX, const double* BETA, double* Y, const blas_int* INCY);
 
-void arma_fortran(arma_dsymv)(char* UPLO, blas_int* N, double* ALPHA, double* A, blas_int* LDA, double* X, blas_int* INCX, double* BETA, double* Y, blas_int* INCY);
+    void arma_fortran(arma_dsymv)(char* UPLO, blas_int* N, double* ALPHA, double* A, blas_int* LDA, double* X, blas_int* INCX, double* BETA, double* Y, blas_int* INCY);
 
-void arma_fortran(arma_ssbmv)(const char* UPLO, const blas_int* N, const blas_int* K, const float* ALPHA, const float* A, const blas_int* LDA, const float* X, const blas_int* INCX, const float* BETA, float* Y, const blas_int* INCY);
+    void arma_fortran(arma_ssbmv)(const char* UPLO, const blas_int* N, const blas_int* K, const float* ALPHA, const float* A, const blas_int* LDA, const float* X, const blas_int* INCX, const float* BETA, float* Y, const blas_int* INCY);
 
-void arma_fortran(arma_dsbmv)(const char* UPLO, const blas_int* N, const blas_int* K, const double* ALPHA, const double* A, const blas_int* LDA, const double* X, const blas_int* INCX, const double* BETA, double* Y, const blas_int* INCY);
+    void arma_fortran(arma_dsbmv)(const char* UPLO, const blas_int* N, const blas_int* K, const double* ALPHA, const double* A, const blas_int* LDA, const double* X, const blas_int* INCX, const double* BETA, double* Y, const blas_int* INCY);
 
-void arma_fortran(arma_sspmv)(const char* UPLO, const blas_int* N, const float* ALPHA, const float* AP, const float* X, const blas_int* INCX, const float* BETA, float* Y, const blas_int* INCY);
+    void arma_fortran(arma_sspmv)(const char* UPLO, const blas_int* N, const float* ALPHA, const float* AP, const float* X, const blas_int* INCX, const float* BETA, float* Y, const blas_int* INCY);
 
-void arma_fortran(arma_dspmv)(const char* UPLO, const blas_int* N, const double* ALPHA, const double* AP, const double* X, const blas_int* INCX, const double* BETA, double* Y, const blas_int* INCY);
+    void arma_fortran(arma_dspmv)(const char* UPLO, const blas_int* N, const double* ALPHA, const double* AP, const double* X, const blas_int* INCX, const double* BETA, double* Y, const blas_int* INCY);
 
-// symmetric matrix
-void arma_fortran(arma_dsysv)(char* UPLO, blas_int* N, blas_int* NRHS, double* A, blas_int* LDA, blas_int* IPIV, double* B, blas_int* LDB, double* WORK, blas_int* LWORK, blas_int* INFO);
+    // symmetric matrix
+    void arma_fortran(arma_dsysv)(char* UPLO, blas_int* N, blas_int* NRHS, double* A, blas_int* LDA, blas_int* IPIV, double* B, blas_int* LDB, double* WORK, blas_int* LWORK, blas_int* INFO);
 
-void arma_fortran(arma_dsygvx)(blas_int* ITYPE, char* JOBZ, char* RANGE, char* UPLO, blas_int* N, double* A, blas_int* LDA, double* B, blas_int* LDB, double* VL, double* VU, blas_int* IL, blas_int* IU, double* ABSTOL, blas_int* M, double* W, double* Z, blas_int* LDZ, double* WORK, blas_int* LWORK, blas_int* IWORK, blas_int* IFAIL, blas_int* INFO);
+    void arma_fortran(arma_dsygvx)(blas_int* ITYPE, char* JOBZ, char* RANGE, char* UPLO, blas_int* N, double* A, blas_int* LDA, double* B, blas_int* LDB, double* VL, double* VU, blas_int* IL, blas_int* IU, double* ABSTOL, blas_int* M, double* W, double* Z, blas_int* LDZ, double* WORK, blas_int* LWORK, blas_int* IWORK, blas_int* IFAIL, blas_int* INFO);
 
-// symmetric positive definite band matrix
-void arma_fortran(arma_spbsv)(const char* UPLO, const blas_int* N, const blas_int* KD, const blas_int* NRHS, float* AB, const blas_int* LDAB, float* B, const blas_int* LDB, blas_int* INFO);
+    // symmetric positive definite band matrix
+    void arma_fortran(arma_spbsv)(const char* UPLO, const blas_int* N, const blas_int* KD, const blas_int* NRHS, float* AB, const blas_int* LDAB, float* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_dpbsv)(const char* UPLO, const blas_int* N, const blas_int* KD, const blas_int* NRHS, double* AB, const blas_int* LDAB, double* B, const blas_int* LDB, blas_int* INFO);
+    void arma_fortran(arma_dpbsv)(const char* UPLO, const blas_int* N, const blas_int* KD, const blas_int* NRHS, double* AB, const blas_int* LDAB, double* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_spbtrs)(const char* UPLO, const blas_int* N, const blas_int* KD, const blas_int* NRHS, float* AB, const blas_int* LDAB, float* B, const blas_int* LDB, blas_int* INFO);
+    void arma_fortran(arma_spbtrs)(const char* UPLO, const blas_int* N, const blas_int* KD, const blas_int* NRHS, float* AB, const blas_int* LDAB, float* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_dpbtrs)(const char* UPLO, const blas_int* N, const blas_int* KD, const blas_int* NRHS, double* AB, const blas_int* LDAB, double* B, const blas_int* LDB, blas_int* INFO);
+    void arma_fortran(arma_dpbtrs)(const char* UPLO, const blas_int* N, const blas_int* KD, const blas_int* NRHS, double* AB, const blas_int* LDAB, double* B, const blas_int* LDB, blas_int* INFO);
 
-// symmetric matrix in packed format
-void arma_fortran(arma_sspsv)(const char* UPLO, const blas_int* N, const blas_int* NRHS, float* AP, blas_int* IPIV, float* B, const blas_int* LDB, blas_int* INFO);
+    // symmetric matrix in packed format
+    void arma_fortran(arma_sspsv)(const char* UPLO, const blas_int* N, const blas_int* NRHS, float* AP, blas_int* IPIV, float* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_dspsv)(const char* UPLO, const blas_int* N, const blas_int* NRHS, double* AP, blas_int* IPIV, double* B, const blas_int* LDB, blas_int* INFO);
+    void arma_fortran(arma_dspsv)(const char* UPLO, const blas_int* N, const blas_int* NRHS, double* AP, blas_int* IPIV, double* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_ssptrf)(const char* UPLO, const blas_int* N, float* AP, blas_int* IPIV, blas_int* INFO);
+    void arma_fortran(arma_ssptrf)(const char* UPLO, const blas_int* N, float* AP, blas_int* IPIV, blas_int* INFO);
 
-void arma_fortran(arma_dsptrf)(const char* UPLO, const blas_int* N, double* AP, blas_int* IPIV, blas_int* INFO);
+    void arma_fortran(arma_dsptrf)(const char* UPLO, const blas_int* N, double* AP, blas_int* IPIV, blas_int* INFO);
 
-void arma_fortran(arma_ssptrs)(const char* UPLO, const blas_int* N, const blas_int* NRHS, const float* AP, const blas_int* IPIV, float* B, const blas_int* LDB, blas_int* INFO);
+    void arma_fortran(arma_ssptrs)(const char* UPLO, const blas_int* N, const blas_int* NRHS, const float* AP, const blas_int* IPIV, float* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_dsptrs)(const char* UPLO, const blas_int* N, const blas_int* NRHS, const double* AP, const blas_int* IPIV, double* B, const blas_int* LDB, blas_int* INFO);
+    void arma_fortran(arma_dsptrs)(const char* UPLO, const blas_int* N, const blas_int* NRHS, const double* AP, const blas_int* IPIV, double* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_ssptri)(const char* UPLO, const blas_int* N, float* AP, const blas_int* IPIV, float* WORK, blas_int* INFO);
+    void arma_fortran(arma_ssptri)(const char* UPLO, const blas_int* N, float* AP, const blas_int* IPIV, float* WORK, blas_int* INFO);
 
-void arma_fortran(arma_dsptri)(const char* UPLO, const blas_int* N, double* AP, const blas_int* IPIV, double* WORK, blas_int* INFO);
+    void arma_fortran(arma_dsptri)(const char* UPLO, const blas_int* N, double* AP, const blas_int* IPIV, double* WORK, blas_int* INFO);
 
-void arma_fortran(arma_sppsv)(const char* UPLO, const blas_int* N, const blas_int* NRHS, float* AP, float* B, const blas_int* LDB, blas_int* INFO);
+    void arma_fortran(arma_sppsv)(const char* UPLO, const blas_int* N, const blas_int* NRHS, float* AP, float* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_dppsv)(const char* UPLO, const blas_int* N, const blas_int* NRHS, double* AP, double* B, const blas_int* LDB, blas_int* INFO);
+    void arma_fortran(arma_dppsv)(const char* UPLO, const blas_int* N, const blas_int* NRHS, double* AP, double* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_spptrf)(const char* UPLO, const blas_int* N, float* AP, blas_int* INFO);
+    void arma_fortran(arma_spptrf)(const char* UPLO, const blas_int* N, float* AP, blas_int* INFO);
 
-void arma_fortran(arma_dpptrf)(const char* UPLO, const blas_int* N, double* AP, blas_int* INFO);
+    void arma_fortran(arma_dpptrf)(const char* UPLO, const blas_int* N, double* AP, blas_int* INFO);
 
-void arma_fortran(arma_spptrs)(const char* UPLO, const blas_int* N, const blas_int* NRHS, const float* AP, float* B, const blas_int* LDB, blas_int* INFO);
+    void arma_fortran(arma_spptrs)(const char* UPLO, const blas_int* N, const blas_int* NRHS, const float* AP, float* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_dpptrs)(const char* UPLO, const blas_int* N, const blas_int* NRHS, const double* AP, double* B, const blas_int* LDB, blas_int* INFO);
+    void arma_fortran(arma_dpptrs)(const char* UPLO, const blas_int* N, const blas_int* NRHS, const double* AP, double* B, const blas_int* LDB, blas_int* INFO);
 
-void arma_fortran(arma_spptri)(const char* UPLO, const blas_int* N, float* AP, float* WORK, blas_int* INFO);
+    void arma_fortran(arma_spptri)(const char* UPLO, const blas_int* N, float* AP, float* WORK, blas_int* INFO);
 
-void arma_fortran(arma_dpptri)(const char* UPLO, const blas_int* N, double* AP, double* WORK, blas_int* INFO);
+    void arma_fortran(arma_dpptri)(const char* UPLO, const blas_int* N, double* AP, double* WORK, blas_int* INFO);
 
-void arma_fortran(arma_dsgesv)(blas_int*, blas_int*, double*, blas_int*, blas_int*, double*, blas_int*, double*, blas_int*, double*, float*, blas_int*, blas_int*);
+    void arma_fortran(arma_dsgesv)(blas_int*, blas_int*, double*, blas_int*, blas_int*, double*, blas_int*, double*, blas_int*, double*, float*, blas_int*, blas_int*);
 
-void arma_fortran(arma_dsposv)(const char*, blas_int*, blas_int*, double*, blas_int*, double*, blas_int*, double*, blas_int*, double*, float*, blas_int*, blas_int*);
-}
+    void arma_fortran(arma_dsposv)(const char*, blas_int*, blas_int*, double*, blas_int*, double*, blas_int*, double*, blas_int*, double*, float*, blas_int*, blas_int*);
+    }
+} // namespace arma
