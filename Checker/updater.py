641b2e492e84b1c016f5b18d2fc3b8fe271f9eb4
Theodore Chang <tlcfem@gmail.com>
2025-09-06 21:08:12 +0200

    Add missing copyright notice and improve code formatting in updater scripts

--- Checker/updater.py
+++ Checker/updater.py
@@ -1,3 +1,18 @@
+#  Copyright (C) 2017-2025 Theodore Chang
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import re
 import sys
 import urllib.request
@@ -5,10 +20,14 @@ from os.path import abspath
 
 
 def check_version(_major: int, _minor: int, _patch: int):
-    html = urllib.request.urlopen(
-        "https://github.com/TLCFEM/suanPan/releases").read()
+    BASE_URL: str = "https://github.com/TLCFEM/suanPan/releases"
 
-    version = re.search(r'suanPan-v(\d)\.(\d)\.?(\d)?', html.decode('utf-8'))
+    try:
+        html = urllib.request.urlopen(BASE_URL).read()
+    except Exception:
+        return
+
+    version = re.search(r"suanPan-v(\d)\.(\d)\.?(\d)?", html.decode("utf-8"))
 
     if not version:
         return
@@ -17,20 +36,21 @@ def check_version(_major: int, _minor: int, _patch: int):
     new_minor = int(version.group(2))
     new_patch = int(version.group(3)) if version.group(3) else 0
 
-    if 100 * new_major + 10 * new_minor + new_patch <= 100 * _major + 10 * _minor + _patch:
+    if (
+        100 * new_major + 10 * new_minor + new_patch
+        <= 100 * _major + 10 * _minor + _patch
+    ):
         return
 
-    url = f'https://github.com/TLCFEM/suanPan/releases/download/{version.group(0)}/'
+    result = input(f"New version {version.group(0)} available, download now? [y/N] ")
 
-    result = input(f'New version {version.group(0)} available, download now? [y/N] ')
-
-    if result == '' or result[0] != 'y' and result[0] != 'Y':
+    if result == "" or result[0] != "y" and result[0] != "Y":
         return
 
-    print('\nDownload the new version:')
+    print("\nDownload the new version:")
 
     version_list = []
-    if sys.platform.startswith('win32'):
+    if sys.platform.startswith("win32"):
         version_list = [
             "suanPan-win-mkl-no-avx.zip",
             "suanPan-win-mkl-vtk-no-avx.zip",
@@ -41,7 +61,7 @@ def check_version(_major: int, _minor: int, _patch: int):
             "suanPan-win-openblas-vtk.7z",
             "suanPan-win-openblas.7z",
         ]
-    elif sys.platform.startswith('linux'):
+    elif sys.platform.startswith("linux"):
         version_list = [
             "suanPan-linux-mkl-no-avx.tar.gz",
             "suanPan-linux-mkl-vtk-no-avx.tar.gz",
@@ -56,16 +76,18 @@ def check_version(_major: int, _minor: int, _patch: int):
             "suanPan-linux-openblas-vtk.tar.gz",
             "suanPan-linux-openblas.tar.gz",
         ]
-    elif sys.platform.startswith('darwin'):
+    elif sys.platform.startswith("darwin"):
         version_list = [
             "suanPan-macos-openblas-vtk.tar.gz",
             "suanPan-macos-openblas.tar.gz",
         ]
 
     for index, item in enumerate(version_list):
-        print(f'  [{index}] {item}')
+        print(f"  [{index}] {item}")
 
-    result = input("\nPlease select the version you want to download (leave empty to exit): ")
+    result = input(
+        "\nPlease select the version you want to download (leave empty to exit): "
+    )
 
     if not result.isdigit():
         return
@@ -77,13 +99,17 @@ def check_version(_major: int, _minor: int, _patch: int):
 
     file_name = version_list[result]
 
-    urllib.request.urlretrieve(url + file_name, file_name)
+    try:
+        urllib.request.urlretrieve(
+            f"{BASE_URL}/download/{version.group(0)}/{file_name}", file_name
+        )
+        print(f"\nDownloaded {abspath(file_name)}.")
+        print("You can manually extract the archive to overwrite the existing folder.")
+    except Exception:
+        print("Download failed.")
 
-    print(f"\nDownloaded {abspath(file_name)}")
-    print("You can manually extract the archive to overwrite the existing folder.")
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) == 3:
         major, minor, patch = int(sys.argv[1]), int(sys.argv[2]), 0
     elif len(sys.argv) == 4:
