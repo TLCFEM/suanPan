847bf2d7bec3482502476b7568c439dd9a46c59b
Theodore Chang <tlcfem@gmail.com>
2025-09-27 04:34:48 +0200

    Add Balloon1D material class and include in CMakeLists.txt

---
+++ Material/Material1D/vonMises/Balloon1D.cpp
@@ -0,0 +1,183 @@
+/*******************************************************************************
+ * Copyright (C) 2017-2025 Theodore Chang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#include "Balloon1D.h"
+
+#include <Domain/DomainBase.h>
+#include <Domain/Factory.hpp>
+
+const double DataBalloon1D::Saturation::root_one_half = sqrt(1.5);
+
+const double Balloon1D::rate_bound = -log(z_bound);
+
+pod2 Balloon1D::yield_ratio(const double z) {
+    if(z < z_bound) return {rate_bound, 0.};
+
+    return {-log(z), -1. / z};
+}
+
+Balloon1D::Balloon1D(const unsigned T, DataBalloon1D&& D, const double R)
+    : DataBalloon1D{std::move(D)}
+    , Material1D(T, R) {}
+
+int Balloon1D::initialize(const shared_ptr<DomainBase>&) {
+    trial_stiffness = current_stiffness = initial_stiffness = elastic;
+
+    initialize_history(5u + static_cast<unsigned>(b.size() + c.size()));
+
+    return SUANPAN_SUCCESS;
+}
+
+unique_ptr<Material> Balloon1D::get_copy() { return std::make_unique<Balloon1D>(*this); }
+
+int Balloon1D::update_trial_status(const vec& t_strain) {
+    incre_strain = (trial_strain = t_strain) - current_strain;
+
+    if(std::fabs(incre_strain(0)) <= datum::eps) return SUANPAN_SUCCESS;
+
+    trial_stress = current_stress + (trial_stiffness = initial_stiffness) * incre_strain;
+
+    trial_history = current_history;
+    const auto& current_q = current_history(1);
+    const auto& current_qm = current_history(2);
+    const auto& current_qr = current_history(3);
+    const auto& current_z = current_history(4);
+    auto& iteration = trial_history(0);
+    auto& q = trial_history(1);
+    auto& qm = trial_history(2);
+    auto& qr = trial_history(3);
+    auto& z = trial_history(4);
+
+    const vec current_alpha(&current_history(5), b.size(), false, true);
+    const vec current_d(&current_history(5 + b.size()), c.size(), false, true);
+    vec alpha(&trial_history(5), b.size(), false, true);
+    vec d(&trial_history(5 + b.size()), c.size(), false, true);
+
+    iteration = 0.;
+    auto gamma = 0., ref_error = 0.;
+    auto start_z = current_z;
+
+    vec2 residual, incre;
+    mat22 jacobian;
+
+    auto counter = 0u;
+    while(true) {
+        if(max_iteration == ++counter) {
+            suanpan_error("Cannot converge within {} iterations.\n", max_iteration);
+            return SUANPAN_FAIL;
+        }
+
+        q = current_q + gamma;
+        qm = current_qm + gamma;
+        qr = current_qr + gamma;
+
+        const auto exp_iso = saturation_iso * std::exp(-m_iso * q);
+        auto y = initial_iso + saturation_iso + k_iso * q - exp_iso;
+        auto dy = k_iso + m_iso * exp_iso;
+        if(y < 0.) y = dy = 0.;
+
+        const auto exp_kin = saturation_kin * std::exp(-m_kin * q);
+        auto a = initial_kin + saturation_kin + k_kin * q - exp_kin;
+        auto da = k_kin + m_kin * exp_kin;
+        if(a < 0.) a = da = 0.;
+
+        vec bottom_alpha(b.size(), fill::none), bottom_d(c.size(), fill::none);
+        for(auto I = 0llu; I < b.size(); ++I) bottom_alpha(I) = 1. + b[I].r() * gamma;
+        for(auto I = 0llu; I < c.size(); ++I) bottom_d(I) = 1. + c[I].r() * gamma;
+
+        const auto n = trial_stress(0) - a * accu(current_alpha / bottom_alpha) + (z - 1.) * y * accu(current_d / bottom_d) > 0. ? 1. : -1.;
+
+        for(auto I = 0llu; I < b.size(); ++I) alpha(I) = (b[I].rb() * gamma * n + current_alpha(I)) / bottom_alpha(I);
+        for(auto I = 0llu; I < c.size(); ++I) d(I) = (c[I].rb() * gamma * n + current_d(I)) / bottom_d(I);
+
+        const auto sum_alpha = accu(alpha), sum_d = accu(d);
+
+        if(1u == counter) {
+            const auto s = (y * sum_d + a * sum_alpha - current_stress(0)) / (trial_stress(0) - current_stress(0));
+            if(s >= 1.) {
+                z = ((trial_stress(0) - a * sum_alpha) / y - sum_d) / (n - sum_d);
+                return SUANPAN_SUCCESS;
+            }
+            if(s > 0.) start_z = 0.;
+        }
+
+        auto dalpha = 0., dd = 0.;
+        for(auto I = 0llu; I < b.size(); ++I) dalpha += b[I].r() * (b[I].b() * n - alpha[I]) / bottom_alpha[I];
+        for(auto I = 0llu; I < c.size(); ++I) dd += c[I].r() * (c[I].b() * n - d[I]) / bottom_d[I];
+
+        const auto trial_ratio = yield_ratio(z);
+        const auto avg_rate = u * trial_ratio[0];
+
+        residual(0) = std::fabs(trial_stress(0) - elastic * gamma * n - a * sum_alpha + (z - 1.) * y * sum_d) - z * y;
+        residual(1) = z - start_z - gamma * avg_rate;
+
+        jacobian(0, 0) = n * ((z - 1.) * (y * dd + sum_d * dy) - (a * dalpha + sum_alpha * da)) - elastic - z * dy;
+        jacobian(0, 1) = n * y * sum_d - y;
+
+        jacobian(1, 0) = -avg_rate;
+        jacobian(1, 1) = 1. - u * gamma * trial_ratio[1];
+
+        if(!solve(incre, jacobian, residual)) return SUANPAN_FAIL;
+
+        const auto error = inf_norm(incre);
+        if(1u == counter) ref_error = error;
+        suanpan_debug("Local iteration error: {:.5E}.\n", error);
+        if(error < tolerance * ref_error || ((error < tolerance || inf_norm(residual) < tolerance) && counter > 5u)) {
+            iteration = counter;
+            trial_stress -= elastic * gamma * n;
+            trial_stiffness += elastic / det(jacobian) * elastic * jacobian(1, 1);
+            return SUANPAN_SUCCESS;
+        }
+
+        gamma -= incre(0);
+        if(gamma < 0.) gamma = 0.;
+
+        z = suanpan::clamp_unit(z - incre(1));
+    }
+}
+
+int Balloon1D::clear_status() {
+    current_strain.zeros();
+    current_stress.zeros();
+    current_zr.zeros();
+    current_history = initial_history;
+    current_stiffness = initial_stiffness;
+    return reset_status();
+}
+
+int Balloon1D::commit_status() {
+    current_strain = trial_strain;
+    current_stress = trial_stress;
+    current_zr = trial_zr;
+    current_history = trial_history;
+    current_stiffness = trial_stiffness;
+    return SUANPAN_SUCCESS;
+}
+
+int Balloon1D::reset_status() {
+    trial_strain = current_strain;
+    trial_stress = current_stress;
+    trial_zr = current_zr;
+    trial_history = current_history;
+    trial_stiffness = current_stiffness;
+    return SUANPAN_SUCCESS;
+}
+
+void Balloon1D::print() {
+    suanpan_info("A uniaxial combined hardening material using subloading surface model. doi:10.1007/s00707-025-04339-0\n");
+    Material1D::print();
+}
---
+++ Material/Material1D/vonMises/Balloon1D.h
@@ -0,0 +1,119 @@
+/*******************************************************************************
+ * Copyright (C) 2017-2025 Theodore Chang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+/**
+ * @class Balloon1D
+ * @brief A Balloon1D material class.
+ * @author tlc
+ * @date 27/09/2025
+ * @version 0.1.0
+ * @file Balloon1D.h
+ * @addtogroup Material-1D
+ * @{
+ */
+
+#ifndef BALLOON1D_H
+#define BALLOON1D_H
+
+#include <Material/Material1D/Material1D.h>
+
+struct DataBalloon1D {
+    class Saturation {
+        static const double root_one_half;
+
+        const double rate, bound;
+
+    public:
+        Saturation(const double R, const double B)
+            : rate(R)
+            , bound(B) {}
+
+        [[nodiscard]] double r() const { return rate * root_one_half; }
+
+        [[nodiscard]] double b() const { return bound; }
+
+        [[nodiscard]] double rb() const { return r() * b(); }
+    };
+
+    const double elastic; // elastic modulus
+    const double initial_iso, k_iso, saturation_iso, m_iso;
+    const double initial_kin, k_kin, saturation_kin, m_kin;
+    const double u;
+
+    const unsigned zr_size;
+
+    const std::vector<Saturation> b, c;
+};
+
+class Balloon1D final : protected DataBalloon1D, public Material1D {
+    static constexpr unsigned max_iteration = 20u;
+    static constexpr double z_bound = 1E-15;
+    static const double rate_bound;
+
+    static pod2 yield_ratio(double);
+
+    class memory {
+        std::vector<double> buffer;
+        std::size_t head;
+
+    public:
+        memory(const std::size_t size)
+            : buffer(size, 0.)
+            , head(0) {}
+
+        auto& enqueue(const double value) {
+            buffer[head] = value;
+            head = (head + 1) % buffer.size();
+            return *this;
+        }
+
+        auto zeros() {
+            std::fill(buffer.begin(), buffer.end(), 0.);
+            head = 0;
+        }
+
+        auto max() { return *std::max_element(buffer.cbegin(), buffer.cend()); }
+        auto min() { return *std::min_element(buffer.cbegin(), buffer.cend()); }
+        auto mean() { return std::accumulate(buffer.cbegin(), buffer.cend(), 0.) / buffer.size(); }
+    };
+
+    memory current_zr{zr_size}, trial_zr{zr_size};
+
+    auto push_zr(const double zr) { return (trial_zr = current_zr).enqueue(zr).mean(); }
+
+public:
+    Balloon1D(
+        unsigned,        // tag
+        DataBalloon1D&&, // data
+        double = 0.      // density
+    );
+
+    int initialize(const shared_ptr<DomainBase>&) override;
+
+    unique_ptr<Material> get_copy() override;
+
+    int update_trial_status(const vec&) override;
+
+    int clear_status() override;
+    int commit_status() override;
+    int reset_status() override;
+
+    void print() override;
+};
+
+#endif
+
+//! @}
