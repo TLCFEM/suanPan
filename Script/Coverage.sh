6f854f0ad4521271492d65c06886b1e0cb7dce77
Theodore Chang <tlcfem@gmail.com>
2025-10-17 03:40:48 +0200

    Update Coverage.sh to simplify logging and improve result handling

--- Script/Coverage.sh
+++ Script/Coverage.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 <build_folder> <log_file>"
+  echo "Usage: $0 <build_folder>"
   exit 1
 fi
 
@@ -21,37 +21,31 @@ fi
 
 mapfile -t files < <(find ../Example -name "*.supan")
 
-if [ $# -eq 2 ]; then
-  log_file=$(realpath "$2")
-else
-  log_file="/dev/null"
-fi
-
 declare -A timings
 
-true >"$log_file"
+ROOTDIR=$(pwd)
 
-current_dir=$(pwd)
+mkdir -p Results
 
 for file in "${files[@]}"; do
-  echo "Processing $file ..." | tee -a "$log_file"
+  echo "Processing $file ..."
 
-  file_path=$(realpath "$file")
-  file_base=$(basename "$file_path")
+  FILEPATH=$(realpath "$file")
+  FILEBASE=$(basename "$FILEPATH")
 
-  cd $(dirname "$file_path") || exit 1
+  cd $(dirname "$FILEPATH") || exit 1
 
   exec 3>&1 4>&2
-  TIME_OUTPUT=$({ /usr/bin/time -f "%e" "$current_dir/suanPan" -f "$file_base" >>"$log_file"; } 2>&1 1>&3)
+  TIME_OUTPUT=$({ /usr/bin/time -f "%e" "$ROOTDIR/suanPan" -nc -f "$FILEBASE" >>"$ROOTDIR/Results/$FILEBASE"; } 2>&1 1>&3)
   exec 3>&- 4>&-
-
-  cd "$current_dir" || exit 1
 
   # shellcheck disable=SC2181
   if [ $? -ne 0 ]; then
     echo "Error processing $file." >&2
     exit 1
   fi
+
+  cd "$ROOTDIR" || exit 1
 
   timings["$file"]="$TIME_OUTPUT"
 done
@@ -73,7 +67,9 @@ done
   ./suanPan -ct ~"Large Mixed Precision" ~"Large Sparse Solve Type"
   ./suanPan -c -f ../Example/Misc/Converter/TEST.inp
   ./suanPan -s -f ../Example/Misc/Converter/TEST.inp
-} >>"$log_file"
+} >>/dev/null 2>&1
 
 rm ../Example/Misc/Converter/TEST_out.inp
 rm ../Example/Misc/Converter/TEST_out.supan
+
+tar -czf Results.tar.gz Results
