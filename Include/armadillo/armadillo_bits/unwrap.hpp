562a3140db94dc3a81c4003c8b0f93e153c62b41
Theodore Chang <tlcfem@gmail.com>
2025-10-20 10:22:07 +0200

    Bump up version 15.2.0

--- Include/armadillo/armadillo_bits/unwrap.hpp
+++ Include/armadillo/armadillo_bits/unwrap.hpp
@@ -382,19 +382,21 @@ struct quasi_unwrap< subview_row<eT> >
   {
   inline
   quasi_unwrap(const subview_row<eT>& A)
-    : M(A)
+    : sv( A                    )
+    , M ( A, (A.m.n_rows == 1) )
     {
     arma_debug_sigprint();
     }
   
-  Row<eT> M;
+  const subview_row<eT>& sv;
+  const Row<eT>          M;
   
-  static constexpr bool is_const     = false;
-  static constexpr bool has_subview  = false;
-  static constexpr bool has_orig_mem = false;
+  static constexpr bool is_const     = true;
+  static constexpr bool has_subview  = true;
+  static constexpr bool has_orig_mem = false;  // NOTE: set to false as this is the general case; original memory is only used when the subview is a contiguous chunk
   
   template<typename eT2>
-  constexpr bool is_alias(const Mat<eT2>&) const { return false; }
+  arma_inline bool is_alias(const Mat<eT2>& X) const { return (is_same_type<eT,eT2>::yes) && ( (sv.m.n_rows == 1) ? (void_ptr(&X) == void_ptr(&(sv.m))) : false ); }
   };
 
 
@@ -1430,7 +1432,8 @@ struct partial_unwrap< subview_row<eT> >
   
   inline
   partial_unwrap(const subview_row<eT>& A)
-    : M(A)
+    : sv( A                    )
+    , M ( A, (A.m.n_rows == 1) )
     {
     arma_debug_sigprint();
     }
@@ -1438,13 +1441,14 @@ struct partial_unwrap< subview_row<eT> >
   constexpr eT get_val() const { return eT(1); }
   
   template<typename eT2>
-  constexpr bool is_alias(const Mat<eT2>&) const { return false; }
+  arma_inline bool is_alias(const Mat<eT2>& X) const { return (sv.m.n_rows == 1) ? (void_ptr(&X) == void_ptr(&(sv.m))) : false; }
   
   static constexpr bool do_trans = false;
   static constexpr bool do_times = false;
-  static constexpr bool is_fast  = false;
+  static constexpr bool is_fast  = false;  // can't determine at compile time that memory is reused
   
-  const Row<eT> M;
+  const subview_row<eT>& sv;
+  const Row<eT>          M;
   };
 
 
