60cb5f3150fa33da6858a5b3da7acecedcb45ec3
Theodore Chang <tlcfem@gmail.com>
2025-11-21 18:53:26 +0100

    Update build type to Release in CI workflow and set version to 3.9.5 in RPM spec

--- Script/Version.sh
+++ Script/Version.sh
@@ -37,46 +37,52 @@ cd "$SCRIPT_DIR" || exit
 yml_file=$(find ".github/workflows" -name "*.yml")
 for file in $yml_file; do
   if [ -f "$file" ]; then
-    sed -i "s/suanPan-[0-9]\.[0-9]\.[0-9]/suanPan-$1\.$2\.$3/g" "$file"
-    sed -i "s/suanpan.[0-9]\.[0-9]\.[0-9]/suanpan.$1\.$2\.$3/g" "$file"
+    sed -i "s/suanPan-\d\.\d\.\d/suanPan-$1\.$2\.$3/g" "$file"
+    sed -i "s/suanpan.\d\.\d\.\d/suanpan.$1\.$2\.$3/g" "$file"
   fi
 done
 
 # chocolatey
 file_name="Enhancement/suanPan.nuspec"
 if [ -f $file_name ]; then
-  sed -i "s/<version>[0-9]\.[0-9]\.[0-9]<\/version>/<version>$1\.$2\.$3<\/version>/g" $file_name
+  sed -i "s/<version>\d\.\d\.\d<\/version>/<version>$1\.$2\.$3<\/version>/g" $file_name
 fi
 
 # msvc
 file_name="Resource/suanPan.rc"
 if [ -f $file_name ]; then
-  sed -i "s/[0-9],[0-9],[0-9],0/$1,$2,$3,0/g" $file_name
-  sed -i "s/[0-9]\.[0-9]\.[0-9]\.0/$1\.$2\.$3\.0/g" $file_name
+  sed -i "s/\d,\d,\d,0/$1,$2,$3,0/g" $file_name
+  sed -i "s/\d\.\d\.\d\.0/$1\.$2\.$3\.0/g" $file_name
 fi
 
 # application
 file_name="Toolbox/argument.cpp"
 if [ -f $file_name ]; then
-  sed -i "s/constexpr auto SUANPAN_MAJOR = [0-9];/constexpr auto SUANPAN_MAJOR = $1;/g" $file_name
-  sed -i "s/constexpr auto SUANPAN_MINOR = [0-9];/constexpr auto SUANPAN_MINOR = $2;/g" $file_name
-  sed -i "s/constexpr auto SUANPAN_PATCH = [0-9];/constexpr auto SUANPAN_PATCH = $3;/g" $file_name
+  sed -i "s/constexpr auto SUANPAN_MAJOR = \d;/constexpr auto SUANPAN_MAJOR = $1;/g" $file_name
+  sed -i "s/constexpr auto SUANPAN_MINOR = \d;/constexpr auto SUANPAN_MINOR = $2;/g" $file_name
+  sed -i "s/constexpr auto SUANPAN_PATCH = \d;/constexpr auto SUANPAN_PATCH = $3;/g" $file_name
 fi
 
 # cpack
 file_name="CMakeLists.txt"
 if [ -f $file_name ]; then
-  sed -i "s/set(CPACK_PACKAGE_VERSION \"[0-9]\.[0-9]\.[0-9]\")/set(CPACK_PACKAGE_VERSION \"$1\.$2\.$3\")/g" $file_name
+  sed -i "s/set(CPACK_PACKAGE_VERSION \"\d\.\d\.\d\")/set(CPACK_PACKAGE_VERSION \"$1\.$2\.$3\")/g" $file_name
 fi
 
 # snapcraft
 file_name="snapcraft.yaml"
 if [ -f $file_name ]; then
-  sed -i "s/version: \"[0-9]\.[0-9]\.[0-9]\"/version: \"$1\.$2\.$3\"/g" $file_name
+  sed -i "s/version: \"\d\.\d\.\d\"/version: \"$1\.$2\.$3\"/g" $file_name
 fi
 
 # citation
 file_name="CITATION.cff"
 if [ -f $file_name ]; then
-  sed -i "s/version: \"[0-9]\.[0-9]\.[0-9]\"/version: \"$1\.$2\.$3\"/g" $file_name
+  sed -i "s/version: \"\d\.\d\.\d\"/version: \"$1\.$2\.$3\"/g" $file_name
+fi
+
+# rpm spec
+file_name="suanPan.spec.rpkg"
+if [ -f $file_name ]; then
+  sed -i "s/\d\.\d\.\d/$1\.$2\.$3/g" $file_name
 fi
