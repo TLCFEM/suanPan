7bfa10a7970a34ae32a662b5044d5d6a96afffd5
Theodore Chang <tlcfem@gmail.com>
2025-11-22 20:29:00 +0100

    Refactor NodeHelper methods to use static_assert for unimplemented functions and simplify position calculations

--- Domain/NodeHelper.hpp
+++ Domain/NodeHelper.hpp
@@ -21,29 +21,69 @@
 #include "DOF.h"
 #include "Node.h"
 
-template<DOF... D> bool check_dof_definition(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF...> struct always_false {
+    static constexpr bool value = false;
+};
 
-template<DOF... D> vec get_current_position(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> bool check_dof_definition(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
-template<DOF... D> vec get_trial_position(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> vec get_current_position(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
-template<DOF... D> vec get_current_displacement(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> vec get_trial_position(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
-template<DOF... D> vec get_current_velocity(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> vec get_current_displacement(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
-template<DOF... D> vec get_current_acceleration(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> vec get_current_velocity(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
-template<DOF... D> vec get_trial_displacement(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> vec get_current_acceleration(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
-template<DOF... D> vec get_trial_velocity(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> vec get_trial_displacement(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
-template<DOF... D> vec get_trial_acceleration(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> vec get_trial_velocity(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
-template<DOF... D> vec get_incre_displacement(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> vec get_trial_acceleration(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
-template<DOF... D> vec get_incre_velocity(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> vec get_incre_displacement(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
-template<DOF... D> vec get_incre_acceleration(const shared_ptr<Node>&) { throw std::logic_error("not implemented"); }
+template<DOF... D> vec get_incre_velocity(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
+
+template<DOF... D> vec get_incre_acceleration(const shared_ptr<Node>&) {
+    static_assert(always_false<D...>::value, "not implemented");
+    return {};
+}
 
 template<> inline bool check_dof_definition<DOF::U1>(const shared_ptr<Node>& t_node) {
     auto& t_dof = t_node->get_dof_identifier();
@@ -60,98 +100,70 @@ template<> inline bool check_dof_definition<DOF::U1, DOF::U2, DOF::U3>(const sha
     return t_dof.size() > 2 && t_dof.at(0) == DOF::U1 && t_dof.at(1) == DOF::U2 && t_dof.at(2) == DOF::U3;
 }
 
-template<> inline vec get_current_position<DOF::U1>(const shared_ptr<Node>& t_node) {
-    vec t_vec = t_node->get_current_displacement().head(1);
-    if(auto& t_coor = t_node->get_coordinate(); !t_coor.empty()) t_vec(0) += t_coor(0);
-    return t_vec;
-}
+template<> inline vec get_current_position<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_displacement()).resize(1) + vec(t_node->get_coordinate()).resize(1); }
 
-template<> inline vec get_trial_position<DOF::U1>(const shared_ptr<Node>& t_node) {
-    vec t_vec = t_node->get_trial_displacement().head(1);
-    if(auto& t_coor = t_node->get_coordinate(); !t_coor.empty()) t_vec(0) += t_coor(0);
-    return t_vec;
-}
+template<> inline vec get_trial_position<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_displacement()).resize(1) + vec(t_node->get_coordinate()).resize(1); }
 
-template<> inline vec get_current_displacement<DOF::U1>(const shared_ptr<Node>& t_node) { return t_node->get_current_displacement().head(1); }
+template<> inline vec get_current_displacement<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_displacement()).resize(1); }
 
-template<> inline vec get_current_velocity<DOF::U1>(const shared_ptr<Node>& t_node) { return t_node->get_current_velocity().head(1); }
+template<> inline vec get_current_velocity<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_velocity()).resize(1); }
 
-template<> inline vec get_current_acceleration<DOF::U1>(const shared_ptr<Node>& t_node) { return t_node->get_current_acceleration().head(1); }
+template<> inline vec get_current_acceleration<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_acceleration()).resize(1); }
 
-template<> inline vec get_trial_displacement<DOF::U1>(const shared_ptr<Node>& t_node) { return t_node->get_trial_displacement().head(1); }
+template<> inline vec get_trial_displacement<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_displacement()).resize(1); }
 
-template<> inline vec get_trial_velocity<DOF::U1>(const shared_ptr<Node>& t_node) { return t_node->get_trial_velocity().head(1); }
+template<> inline vec get_trial_velocity<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_velocity()).resize(1); }
 
-template<> inline vec get_trial_acceleration<DOF::U1>(const shared_ptr<Node>& t_node) { return t_node->get_trial_acceleration().head(1); }
+template<> inline vec get_trial_acceleration<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_acceleration()).resize(1); }
 
-template<> inline vec get_incre_displacement<DOF::U1>(const shared_ptr<Node>& t_node) { return t_node->get_incre_displacement().head(1); }
+template<> inline vec get_incre_displacement<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_incre_displacement()).resize(1); }
 
-template<> inline vec get_incre_velocity<DOF::U1>(const shared_ptr<Node>& t_node) { return t_node->get_incre_velocity().head(1); }
+template<> inline vec get_incre_velocity<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_incre_velocity()).resize(1); }
 
-template<> inline vec get_incre_acceleration<DOF::U1>(const shared_ptr<Node>& t_node) { return t_node->get_incre_acceleration().head(1); }
+template<> inline vec get_incre_acceleration<DOF::U1>(const shared_ptr<Node>& t_node) { return vec(t_node->get_incre_acceleration()).resize(1); }
 
-template<> inline vec get_current_position<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) {
-    const auto& t_coor = t_node->get_coordinate();
-    vec t_vec = t_node->get_current_displacement().head(2);
-    for(auto I = 0llu; I < std::min(2llu, t_coor.n_elem); ++I) t_vec(I) += t_coor(I);
-    return t_vec;
-}
+template<> inline vec get_current_position<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_displacement()).resize(2) + vec(t_node->get_coordinate()).resize(2); }
 
-template<> inline vec get_trial_position<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) {
-    const auto& t_coor = t_node->get_coordinate();
-    vec t_vec = t_node->get_trial_displacement().head(2);
-    for(auto I = 0llu; I < std::min(2llu, t_coor.n_elem); ++I) t_vec(I) += t_coor(I);
-    return t_vec;
-}
+template<> inline vec get_trial_position<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_displacement()).resize(2) + vec(t_node->get_coordinate()).resize(2); }
 
-template<> inline vec get_current_displacement<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return t_node->get_current_displacement().head(2); }
+template<> inline vec get_current_displacement<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_displacement()).resize(2); }
 
-template<> inline vec get_current_velocity<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return t_node->get_current_velocity().head(2); }
+template<> inline vec get_current_velocity<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_velocity()).resize(2); }
 
-template<> inline vec get_current_acceleration<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return t_node->get_current_acceleration().head(2); }
+template<> inline vec get_current_acceleration<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_acceleration()).resize(2); }
 
-template<> inline vec get_trial_displacement<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return t_node->get_trial_displacement().head(2); }
+template<> inline vec get_trial_displacement<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_displacement()).resize(2); }
 
-template<> inline vec get_trial_velocity<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return t_node->get_trial_velocity().head(2); }
+template<> inline vec get_trial_velocity<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_velocity()).resize(2); }
 
-template<> inline vec get_trial_acceleration<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return t_node->get_trial_acceleration().head(2); }
+template<> inline vec get_trial_acceleration<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_acceleration()).resize(2); }
 
-template<> inline vec get_incre_displacement<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return t_node->get_incre_displacement().head(2); }
+template<> inline vec get_incre_displacement<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_incre_displacement()).resize(2); }
 
-template<> inline vec get_incre_velocity<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return t_node->get_incre_velocity().head(2); }
+template<> inline vec get_incre_velocity<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_incre_velocity()).resize(2); }
 
-template<> inline vec get_incre_acceleration<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return t_node->get_incre_acceleration().head(2); }
+template<> inline vec get_incre_acceleration<DOF::U1, DOF::U2>(const shared_ptr<Node>& t_node) { return vec(t_node->get_incre_acceleration()).resize(2); }
 
-template<> inline vec get_current_position<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) {
-    const auto& t_coor = t_node->get_coordinate();
-    vec t_vec = t_node->get_current_displacement().head(3);
-    for(auto I = 0llu; I < std::min(3llu, t_coor.n_elem); ++I) t_vec(I) += t_coor(I);
-    return t_vec;
-}
+template<> inline vec get_current_position<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_displacement()).resize(3) + vec(t_node->get_coordinate()).resize(3); }
 
-template<> inline vec get_trial_position<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) {
-    const auto& t_coor = t_node->get_coordinate();
-    vec t_vec = t_node->get_trial_displacement().head(3);
-    for(auto I = 0llu; I < std::min(3llu, t_coor.n_elem); ++I) t_vec(I) += t_coor(I);
-    return t_vec;
-}
+template<> inline vec get_trial_position<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_displacement()).resize(3) + vec(t_node->get_coordinate()).resize(3); }
 
-template<> inline vec get_current_displacement<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return t_node->get_current_displacement().head(3); }
+template<> inline vec get_current_displacement<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_displacement()).resize(3); }
 
-template<> inline vec get_current_velocity<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return t_node->get_current_velocity().head(3); }
+template<> inline vec get_current_velocity<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_velocity()).resize(3); }
 
-template<> inline vec get_current_acceleration<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return t_node->get_current_acceleration().head(3); }
+template<> inline vec get_current_acceleration<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_current_acceleration()).resize(3); }
 
-template<> inline vec get_trial_displacement<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return t_node->get_trial_displacement().head(3); }
+template<> inline vec get_trial_displacement<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_displacement()).resize(3); }
 
-template<> inline vec get_trial_velocity<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return t_node->get_trial_velocity().head(3); }
+template<> inline vec get_trial_velocity<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_velocity()).resize(3); }
 
-template<> inline vec get_trial_acceleration<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return t_node->get_trial_acceleration().head(3); }
+template<> inline vec get_trial_acceleration<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_trial_acceleration()).resize(3); }
 
-template<> inline vec get_incre_displacement<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return t_node->get_incre_displacement().head(3); }
+template<> inline vec get_incre_displacement<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_incre_displacement()).resize(3); }
 
-template<> inline vec get_incre_velocity<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return t_node->get_incre_velocity().head(3); }
+template<> inline vec get_incre_velocity<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_incre_velocity()).resize(3); }
 
-template<> inline vec get_incre_acceleration<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return t_node->get_incre_acceleration().head(3); }
+template<> inline vec get_incre_acceleration<DOF::U1, DOF::U2, DOF::U3>(const shared_ptr<Node>& t_node) { return vec(t_node->get_incre_acceleration()).resize(3); }
 
 #endif
