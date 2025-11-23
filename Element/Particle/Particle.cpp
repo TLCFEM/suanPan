7f5da154351556d7fff624a7065ca9cbaa71c31c
Theodore Chang <tlcfem@gmail.com>
2025-11-23 05:10:05 +0100

    Add Particle class and update CMakeLists to include Particle module

---
+++ Element/Particle/Particle.cpp
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (C) 2017-2025 Theodore Chang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#include "Particle.h"
+
+#include <Domain/DOF.h>
+
+Particle::Particle(const unsigned T, const unsigned N, std::vector<DOF>&& D)
+    : Element(T, 1, D.size(), uvec{N}, std::move(D)) {}
+
+SphericalParticle::SphericalParticle(const unsigned T, const unsigned N, std::vector<DOF>&& D, const double R, const double E, const double V, const double M, const double I)
+    : Particle(T, N, std::move(D))
+    , radius(R)
+    , elastic_modulus(E)
+    , poisson_ratio(V)
+    , mass(M)
+    , inertia(I) {}
+
+InertialSphericalParticle2D::InertialSphericalParticle2D(const unsigned T, const unsigned N, const double R, const double E, const double V, const double M, const double I)
+    : SphericalParticle(T, N, {DOF::U1, DOF::U2, DOF::UR3}, R, E, V, M, I) {}
+
+SphericalParticle2D::SphericalParticle2D(const unsigned T, const unsigned N, const double R, const double E, const double V, const double M)
+    : SphericalParticle(T, N, {DOF::U1, DOF::U2}, R, E, V, M, 0.) {}
---
+++ Element/Particle/Particle.h
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (C) 2017-2025 Theodore Chang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+/**
+ * @class Particle
+ * @brief The Particle class represents the abstract particle used in DEM.
+ *
+ * @author tlc
+ * @date 23/11/2025
+ * @version 0.1.0
+ * @file Particle.h
+ * @addtogroup Particle
+ * @ingroup Element
+ * @{
+ */
+
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include <Element/Element.h>
+
+class Particle : public Element {
+public:
+    Particle(
+        unsigned,          // tag
+        unsigned,          // node tag
+        std::vector<DOF>&& // dof identifier
+    );
+
+    int update_status() override { return SUANPAN_SUCCESS; }
+
+    int commit_status() override { return SUANPAN_SUCCESS; }
+    int clear_status() override { return SUANPAN_SUCCESS; }
+    int reset_status() override { return SUANPAN_SUCCESS; }
+};
+
+class SphericalParticle : public Particle {
+    const double radius, elastic_modulus, poisson_ratio, mass, inertia;
+
+public:
+    SphericalParticle(
+        unsigned,           // tag
+        unsigned,           // node tag
+        std::vector<DOF>&&, // dof identifier
+        double,             // radius
+        double,             // elastic modulus
+        double,             // poisson ratio
+        double,             // mass
+        double              // inertia
+    );
+};
+
+class InertialSphericalParticle2D : public SphericalParticle {
+public:
+    InertialSphericalParticle2D(
+        unsigned, // tag
+        unsigned, // node tag
+        double,   // radius
+        double,   // elastic modulus
+        double,   // poisson ratio
+        double,   // mass
+        double    // inertia
+    );
+};
+
+class SphericalParticle2D : public SphericalParticle {
+public:
+    SphericalParticle2D(
+        unsigned, // tag
+        unsigned, // node tag
+        double,   // radius
+        double,   // elastic modulus
+        double,   // poisson ratio
+        double    // mass
+    );
+};
+
+#endif
+
+//! @}
