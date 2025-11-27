e1715721ec76311df7bf9a04a3e9ab451c4c4a5a
Theodore Chang <tlcfem@gmail.com>
2025-11-27 08:45:31 +0100

    Revise fix commands in multiple element files for consistency and accuracy

--- Load/BodyForce.cpp
+++ Load/BodyForce.cpp
@@ -36,7 +36,7 @@ int BodyForce::process(const shared_ptr<DomainBase>& D) {
 
         vec t_body_load(element->get_dof_number(), fill::zeros);
         for(const auto J : get_dof_component())
-            if(const auto index = static_cast<std::uint8_t>(J); index < element->get_dof_number()) t_body_load(index) = final_load;
+            if(const auto index = static_cast<std::uint8_t>(J) - 1u; index < element->get_dof_number()) t_body_load(index) = final_load;
         if(auto& t_body_force = element->update_body_force(t_body_load); !t_body_force.empty()) trial_load(element->get_dof_encoding()) += t_body_force; };
 
     for(const auto tag : target_element) check(D->get<Element>(tag));
