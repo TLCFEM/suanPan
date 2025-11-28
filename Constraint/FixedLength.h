a311c22f94680c12b602e3cc3eff824abdeee98a
Theodore Chang <tlcfem@gmail.com>
2025-11-29 00:02:09 +0100

    Refactor FixedLength class to use templates for dimension handling, improving flexibility and type safety in constraint definitions.

--- Constraint/FixedLength.h
+++ Constraint/FixedLength.h
@@ -34,61 +34,164 @@
 
 #include "Constraint.h"
 
-class FixedLength : public Constraint {
+#include <Domain/Factory.hpp>
+
+template<unsigned DIM> class FixedLength : public Constraint {
     vec initial_chord;
 
 protected:
-    const unsigned dimension;
-
     bool min_bound = false, max_bound = false;
     double min_gap = 0., max_gap = 0.;
 
     [[nodiscard]] bool validate_node() const final { return true; }
 
 public:
-    FixedLength(unsigned, unsigned, uvec&&);
+    FixedLength(const unsigned T, uvec&& N)
+        : Constraint(T, 0, 2u == DIM ? std::vector{Node::DOF::U1, Node::DOF::U2} : std::vector{Node::DOF::U1, Node::DOF::U2, Node::DOF::U3}, {}, 1) { target_node = std::move(N); }
 
-    int initialize(const shared_ptr<DomainBase>&) override;
+    int initialize(const shared_ptr<DomainBase>& D) override {
+        if(SUANPAN_SUCCESS != Constraint::initialize(D)) return SUANPAN_FAIL;
 
-    int process(const shared_ptr<DomainBase>&) override;
+        initial_chord = D->get<Node>(target_node(1))->initial_position(DIM) - D->get<Node>(target_node(0))->initial_position(DIM);
+
+        set_multiplier_size(0u);
+
+        return SUANPAN_SUCCESS;
+    }
+
+    int process(const shared_ptr<DomainBase>& D) override {
+        auto& W = D->get_factory();
+
+        const uvec dof_i = target_node_dof.head(DIM), dof_j = target_node_dof.tail(DIM);
+
+        const vec t_disp = W->get_trial_displacement()(dof_j) - W->get_trial_displacement()(dof_i);
+        const vec t_chord = initial_chord + t_disp;
+
+        if(const auto t_gap = dot(t_chord, t_chord); min_bound && max_bound) {
+            if(0u == lagrangian_size && t_gap > min_gap && t_gap < max_gap) return SUANPAN_SUCCESS;
+
+            auxiliary_load = (2. * std::sqrt(t_gap) < std::sqrt(min_gap) + std::sqrt(max_gap) ? min_gap : max_gap) - dot(initial_chord, initial_chord);
+        }
+        else if(min_bound && !max_bound) {
+            if(0u == lagrangian_size && t_gap > min_gap) return SUANPAN_SUCCESS;
+
+            auxiliary_load = min_gap - dot(initial_chord, initial_chord);
+        }
+        else if(!min_bound && max_bound) {
+            if(0u == lagrangian_size && t_gap < max_gap) return SUANPAN_SUCCESS;
+
+            auxiliary_load = max_gap - dot(initial_chord, initial_chord);
+        }
+
+        set_multiplier_size(1u);
+
+        auxiliary_stiffness.zeros(W->get_size(), lagrangian_size);
+        auxiliary_resistance = 0.;
+        for(auto I = 0u; I < DIM; ++I) {
+            auxiliary_stiffness(dof_i(I)) = -(auxiliary_stiffness(dof_j(I)) = 2. * t_chord(I));
+            auxiliary_resistance += t_disp(I) * (2. * initial_chord(I) + t_disp(I));
+        }
+
+        stiffness.zeros(target_node_dof.n_elem, target_node_dof.n_elem);
+        const auto t_factor = 2. * trial_lambda(0);
+        for(auto I = 0u; I < DIM; ++I) stiffness(I + DIM, I) = stiffness(I, I + DIM) = -(stiffness(I, I) = stiffness(I + DIM, I + DIM) = t_factor);
+
+        resistance = auxiliary_stiffness * trial_lambda;
+
+        return SUANPAN_SUCCESS;
+    }
 
     [[nodiscard]] bool is_connected() const override { return true; }
 
-    void update_status(const vec&) override;
-    void commit_status() override;
-    void clear_status() override;
-    void reset_status() override;
+    void update_status(const vec& incre_lambda) override { trial_lambda += incre_lambda; };
+    void commit_status() override {
+        current_lambda = trial_lambda;
+        set_multiplier_size(0u);
+    }
+    void clear_status() override {
+        current_lambda = trial_lambda.zeros();
+        set_multiplier_size(0u);
+    }
+    void reset_status() override {
+        trial_lambda = current_lambda;
+        set_multiplier_size(0u);
+    }
 };
 
-class MinimumGap final : public FixedLength {
+template<unsigned DIM> class MinimumGap final : public FixedLength<DIM> {
 public:
-    MinimumGap(unsigned, unsigned, double, uvec&&);
+    MinimumGap(const unsigned T, const double M, uvec&& N)
+        : FixedLength<DIM>(T, std::move(N)) {
+        FixedLength<DIM>::min_bound = true;
+        FixedLength<DIM>::min_gap = M * M;
+    }
 };
 
-class MaximumGap final : public FixedLength {
+template<unsigned DIM> class MaximumGap final : public FixedLength<DIM> {
 public:
-    MaximumGap(unsigned, unsigned, double, uvec&&);
+    MaximumGap(const unsigned T, const double M, uvec&& N)
+        : FixedLength<DIM>(T, std::move(N)) {
+        FixedLength<DIM>::max_bound = true;
+        FixedLength<DIM>::max_gap = M * M;
+    }
 };
 
-class Sleeve final : public FixedLength {
+template<unsigned DIM> class Sleeve final : public FixedLength<DIM> {
 public:
-    Sleeve(unsigned, unsigned, double, double, uvec&&);
+    Sleeve(const unsigned T, const double M1, const double M2, uvec&& N)
+        : FixedLength<DIM>(T, std::move(N)) {
+        FixedLength<DIM>::min_bound = true;
+        FixedLength<DIM>::max_bound = true;
+        FixedLength<DIM>::min_gap = M1 * M1;
+        FixedLength<DIM>::max_gap = M2 * M2;
+    }
 };
 
-class MaxForce final : public FixedLength {
+template<unsigned DIM> class MaxForce final : public FixedLength<DIM> {
     const double max_force;
 
     bool trial_flag = false;
     bool current_flag = false;
 
 public:
-    MaxForce(unsigned, unsigned, double, uvec&&);
+    MaxForce(const unsigned T, const double MF, uvec&& N)
+        : FixedLength<DIM>(T, std::move(N))
+        , max_force(MF) {}
 
-    int process(const shared_ptr<DomainBase>&) override;
+    int process(const shared_ptr<DomainBase>& D) override {
+        if(current_flag) {
+            // if already exceeded, the constraint is not triggered
+            FixedLength<DIM>::set_multiplier_size(0u);
+            return SUANPAN_SUCCESS;
+        }
 
-    void commit_status() override;
-    void clear_status() override;
-    void reset_status() override;
+        if(SUANPAN_SUCCESS != FixedLength<DIM>::process(D)) return SUANPAN_FAIL;
+
+        if(0u == FixedLength<DIM>::lagrangian_size) return SUANPAN_SUCCESS;
+
+        vec nodal_resistance(DIM);
+        for(auto I = 0llu; I < nodal_resistance.n_elem; ++I) nodal_resistance(I) = FixedLength<DIM>::resistance(FixedLength<DIM>::target_node_dof(I));
+
+        if(norm(nodal_resistance) > max_force) {
+            trial_flag = true;
+            FixedLength<DIM>::set_multiplier_size(0u);
+        }
+
+        return SUANPAN_SUCCESS;
+    }
+
+    void commit_status() override {
+        current_flag = trial_flag;
+        FixedLength<DIM>::commit_status();
+    }
+    void clear_status() override {
+        current_flag = trial_flag = false;
+        FixedLength<DIM>::clear_status();
+    }
+    void reset_status() override {
+        trial_flag = current_flag;
+        FixedLength<DIM>::reset_status();
+    }
 };
 
 #endif
