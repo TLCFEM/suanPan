8b870dd6997373f57db757248c1d9525fe5cbdd1
Theodore Chang <tlcfem@gmail.com>
2026-02-08 12:24:53 +0100

    Add parallel solver function declaration for distributed computation

--- Toolbox/arpack.cpp
+++ Toolbox/arpack.cpp
@@ -215,3 +215,84 @@ int eig_solve(cx_vec& eigval, const std::shared_ptr<MetaMat<double>>& K, const u
 
     return INFO;
 }
+
+#ifdef SUANPAN_DISTRIBUTED
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void pdsaupd_(int comm, int* ido, char const* bmat, int n, char const* which, int nev, double tol, double* resid, int ncv, double* v, int ldv, int* iparam, int* ipntr, double* workd, double* workl, int lworkl, int* info);
+void pdseupd_(int comm, int rvec, char const* howmny, int const* select, double* d, double* z, int ldz, double sigma, char const* bmat, int n, char const* which, int nev, double tol, double* resid, int ncv, double* v, int ldv, int* iparam, int* ipntr, double* workd, double* workl, int lworkl, int* info);
+void pdnaupd_(int comm, int* ido, char const* bmat, int n, char const* which, int nev, double tol, double* resid, int ncv, double* v, int ldv, int* iparam, int* ipntr, double* workd, double* workl, int lworkl, int* info);
+void pdneupd_(int comm, int rvec, char const* howmny, int const* select, double* dr, double* di, double* z, int ldz, double sigmar, double sigmai, double* workev, char const* bmat, int n, char const* which, int nev, double tol, double* resid, int ncv, double* v, int ldv, int* iparam, int* ipntr, double* workd, double* workl, int lworkl, int* info);
+
+#ifdef __cplusplus
+}
+#endif
+
+int eig_psolve(vec& eigval, mat& eigvec, const mat_ptr& K, const mat_ptr& M, const unsigned num, const char* WHICH) {
+    static auto BMAT{'G'}; // generalized eigenvalue problem A*x=lambda*B*x
+
+    // const auto comm = MPI_Comm_c2f(comm_world.native_handle());
+
+    blas_int IDO{0}, INFO{0};
+    auto N = static_cast<blas_int>(K->n_cols);
+    auto NEV = std::min(static_cast<blas_int>(num), N - 1);
+    auto TOL{0.};
+    auto NCV = std::min(3 * NEV, N); // use a larger NCV to ensure convergence
+    auto LWORKL = 2 * NCV * (NCV + 8);
+
+    blas_int IPARAM[11]{}, IPNTR[14]{};
+    podarray<double> RESID(N), V(uword(N) * uword(NCV)), WORKD(5 * uword(N)), WORKL(LWORKL);
+
+    IPARAM[0] = 1;    // exact shift
+    IPARAM[2] = 1000; // maximum iteration
+    IPARAM[6] = 4;    // mode 4: K*x=lambda*KG*x
+
+    // we choose shift to be -1 to ensure (M+K) is invertible
+    // since we know the eigenvalues are all positive
+    M += K;
+
+    while(99 != IDO) {
+        arma_fortran(arma_dsaupd)(&IDO, &BMAT, &N, (char*)WHICH, &NEV, &TOL, RESID.memptr(), &NCV, V.memptr(), &N, IPARAM, IPNTR, WORKD.memptr(), WORKL.memptr(), &LWORKL, &INFO);
+        if(0 != INFO) break;
+        // ReSharper disable once CppEntityAssignedButNoRead
+        if(vec Y(WORKD.memptr() + IPNTR[1] - 1, N, false, true); -1 == IDO) {
+            vec X(WORKD.memptr() + IPNTR[0] - 1, N, false, true);
+            X = K * X;
+            INFO = M->solve(Y, X);
+            if(0 != INFO) break;
+        }
+        else if(1 == IDO) {
+            const vec X(WORKD.memptr() + IPNTR[2] - 1, N, false, true);
+            INFO = M->solve(Y, X);
+            if(0 != INFO) break;
+        }
+        else if(2 == IDO) {
+            const vec X(WORKD.memptr() + IPNTR[0] - 1, N, false, true);
+            // ReSharper disable once CppDFAUnusedValue
+            Y = K * X;
+        }
+    }
+
+    if(0 != INFO) {
+        suanpan_error("Error code {} received.\n", INFO);
+        return SUANPAN_FAIL;
+    }
+
+    suanpan_debug("Arnoldi iteration counter: {}.\n", IPARAM[2]);
+
+    static blas_int RVEC{1};
+    static auto HOWMNY{'A'};
+    static auto SIGMA{-1.};
+
+    podarray<blas_int> SELECT(NCV);
+
+    eigval.set_size(NEV);
+    eigvec.set_size(N, NEV);
+
+    arma_fortran(arma_dseupd)(&RVEC, &HOWMNY, SELECT.memptr(), eigval.memptr(), eigvec.memptr(), &N, &SIGMA, &BMAT, &N, (char*)WHICH, &NEV, &TOL, RESID.memptr(), &NCV, V.memptr(), &N, IPARAM, IPNTR, WORKD.memptr(), WORKL.memptr(), &LWORKL, &INFO);
+
+    return INFO;
+}
+#endif
