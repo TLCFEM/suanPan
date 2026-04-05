3999c7ffe799eb9640c724f205609f5a67550d7c
Theodore Chang <tlcfem@gmail.com>
2026-04-05 07:17:27 +0200

    Refactor PQuadTree to use std::variant for child nodes and optimize insertion logic

--- Toolbox/tree/pquadtree.hpp
+++ Toolbox/tree/pquadtree.hpp
@@ -20,6 +20,8 @@
 
 #include "common.hpp"
 
+#include <variant>
+
 #ifdef SUANPAN_MT
 #include <oneapi/tbb/parallel_for.h>
 #endif
@@ -30,57 +32,39 @@ template<std::floating_point T = double, unsigned BUCKET_SIZE = 1> class PQuadTr
 
     const BoundingBox<T> box;
 
-    node_pool nodes;
-
-    std::vector<PQuadTree> children;
+    std::variant<node_pool, std::vector<PQuadTree>> child;
 
     const PQuadTree* parent = nullptr;
 
     void attach(const PQuadTree* in_parent) { parent = in_parent; }
 
-    void split() {
-        const auto dx = T(.5) * box.dimension.x, dy = T(.5) * box.dimension.y;
-        children.clear();
-        children.reserve(4);
-        children.emplace_back(BoundingBox<T>{{box.center.x - dx, box.center.y - dy}, {dx, dy}}).attach(this);
-        children.emplace_back(BoundingBox<T>{{box.center.x + dx, box.center.y - dy}, {dx, dy}}).attach(this);
-        children.emplace_back(BoundingBox<T>{{box.center.x + dx, box.center.y + dy}, {dx, dy}}).attach(this);
-        children.emplace_back(BoundingBox<T>{{box.center.x - dx, box.center.y + dy}, {dx, dy}}).attach(this);
-
-        std::array<node_pool, 4> buckets;
-        for(auto&& node : nodes) {
-            const std::size_t a = node->x > box.center.x, b = node->y > box.center.y;
-            buckets[2 * b + (a ^ b)].push_back(node);
-        }
-        nodes = {};
-
-        for(auto i = 0; i < 4; ++i) buckets[i].shrink_to_fit();
-
-#ifdef SUANPAN_MT
-        tbb::parallel_for(0, 4, [&](const auto i) { children[i].insert(std::move(buckets[i])); });
-#else
-        for(auto i = 0; i < 4; ++i) children[i].insert(std::move(buckets[i]));
-#endif
-    }
-
 public:
     explicit PQuadTree(BoundingBox<T>&& in_box)
         : box(std::move(in_box)) {}
 
-    void insert(node_pool&& child) {
-        nodes = std::move(child);
-        if(nodes.size() > BUCKET_SIZE) split();
-    }
+    template<std::forward_iterator IT> void insert(IT begin, IT end) requires std::is_convertible_v<std::iter_value_t<IT>, node_ptr> { insert(node_pool(begin, end)); }
 
-    template<std::forward_iterator IT> void insert(IT begin, IT end) requires std::is_convertible_v<std::iter_value_t<IT>, node_ptr> {
-        nodes.assign(begin, end);
-        if(nodes.size() > BUCKET_SIZE) split();
-    }
+    void insert(node_pool&& in_child) {
+        if(std::get<node_pool>(child = std::move(in_child)).size() <= BUCKET_SIZE) return;
 
-    unsigned depth() const {
-        auto max_depth = 0u;
-        for(auto&& child : children) max_depth = std::max(max_depth, child.depth());
-        return max_depth + 1u;
+        std::array<node_pool, 4> buckets;
+        for(auto&& node : std::get<node_pool>(child)) buckets[2 * (node->y > box.center.y) + (node->x > box.center.x)].push_back(node);
+        for(auto i = 0; i < 4; ++i) buckets[i].shrink_to_fit();
+
+        auto& subtree = std::get<std::vector<PQuadTree>>(child = std::vector<PQuadTree>());
+        subtree.reserve(4);
+
+        const auto dx = T(.5) * box.dimension.x, dy = T(.5) * box.dimension.y;
+        subtree.emplace_back(BoundingBox<T>{{box.center.x - dx, box.center.y - dy}, {dx, dy}}).attach(this);
+        subtree.emplace_back(BoundingBox<T>{{box.center.x + dx, box.center.y - dy}, {dx, dy}}).attach(this);
+        subtree.emplace_back(BoundingBox<T>{{box.center.x - dx, box.center.y + dy}, {dx, dy}}).attach(this);
+        subtree.emplace_back(BoundingBox<T>{{box.center.x + dx, box.center.y + dy}, {dx, dy}}).attach(this);
+
+#ifdef SUANPAN_MT
+        tbb::parallel_for(0, 4, [&](const auto i) { subtree[i].insert(std::move(buckets[i])); });
+#else
+        for(auto i = 0; i < 4; ++i) subtree[i].insert(std::move(buckets[i]));
+#endif
     }
 };
 
