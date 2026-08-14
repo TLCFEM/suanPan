2b2e68505b682045d2a053e869d659822a934620
Theodore Chang <tlcfem@gmail.com>
2026-08-14 03:44:06 +0200

    Remove couple-related functionality from Material class to streamline implementation

--- Material/Material.cpp
+++ Material/Material.cpp
@@ -22,7 +22,6 @@
 
 Material::Material(const unsigned T, const MaterialType MT, const double D)
     : DataMaterial{.density = fabs(D), .material_type = MT}
-    , DataCoupleMaterial{}
     , CopyableTag(T) {}
 
 double Material::get_density() const { return density; }
@@ -49,8 +48,6 @@ int Material::initialize_base(const shared_ptr<DomainBase>&) {
     return SUANPAN_SUCCESS;
 }
 
-void Material::initialize_couple(const shared_ptr<DomainBase>&) {}
-
 void Material::initialize_history(const unsigned size) {
     if(static_cast<uword>(size) > initial_history.size()) initial_history.resize(size);
 
@@ -63,17 +60,9 @@ void Material::set_initialized(const bool F) const { access::rw(initialized) = F
 
 void Material::set_symmetric(const bool F) const { access::rw(symmetric) = F; }
 
-void Material::set_support_couple(const bool F) const { access::rw(support_couple) = F; }
-
 bool Material::is_initialized() const { return initialized; }
 
 bool Material::is_symmetric() const { return symmetric; }
-
-bool Material::is_support_couple() const { return support_couple; }
-
-void Material::set_characteristic_length(const double L) const { access::rw(characteristic_length) = std::max(datum::eps, L); }
-
-double Material::get_characteristic_length() const { return characteristic_length; }
 
 double Material::get(Parameter) const { return 0.; }
 
@@ -123,20 +112,6 @@ const mat& Material::get_initial_damping() const { return initial_damping; }
 
 const mat& Material::get_initial_inertial() const { return initial_inertial; }
 
-const vec& Material::get_trial_curvature() { return trial_curvature; }
-
-const vec& Material::get_trial_couple_stress() { return trial_couple_stress; }
-
-const mat& Material::get_trial_couple_stiffness() { return trial_couple_stiffness; }
-
-const vec& Material::get_current_curvature() { return current_curvature; }
-
-const vec& Material::get_current_couple_stress() { return current_couple_stress; }
-
-const mat& Material::get_current_couple_stiffness() { return current_couple_stiffness; }
-
-const mat& Material::get_initial_couple_stiffness() const { return initial_couple_stiffness; }
-
 unique_ptr<Material> Material::unique_copy() { throw std::invalid_argument("hidden method unique_copy() called"); }
 
 int Material::update_incre_status(const double i_strain) { return update_incre_status(vec{i_strain}); }
@@ -170,33 +145,6 @@ int Material::update_trial_status(const vec& t_strain, const vec& t_strain_rate,
     return update_trial_status(t_strain);
 }
 
-int Material::update_couple_incre_status(const double i_strain) { return update_couple_incre_status(vec{i_strain}); }
-
-int Material::update_couple_incre_status(const double i_strain, const double i_strain_rate) { return update_couple_incre_status(vec{i_strain}, vec{i_strain_rate}); }
-
-int Material::update_couple_incre_status(const double i_strain, const double i_strain_rate, const double i_strain_acc) { return update_couple_incre_status(vec{i_strain}, vec{i_strain_rate}, vec{i_strain_acc}); }
-
-int Material::update_couple_trial_status(const double t_strain) { return update_couple_trial_status(vec{t_strain}); }
-
-int Material::update_couple_trial_status(const double t_strain, const double t_strain_rate) { return update_couple_trial_status(vec{t_strain}, vec{t_strain_rate}); }
-
-int Material::update_couple_trial_status(const double t_strain, const double t_strain_rate, const double t_strain_acc) { return update_couple_trial_status(vec{t_strain}, vec{t_strain_rate}, vec{t_strain_acc}); }
-
-int Material::update_couple_incre_status(const vec& i_curvature) { return update_couple_trial_status(current_curvature + i_curvature); }
-
-int Material::update_couple_incre_status(const vec& i_curvature, const vec&) { return update_couple_trial_status(current_curvature + i_curvature); }
-
-int Material::update_couple_incre_status(const vec& i_curvature, const vec&, const vec&) { return update_couple_trial_status(current_curvature + i_curvature); }
-
-int Material::update_couple_trial_status(const vec& t_curvature) {
-    trial_couple_stress = trial_couple_stiffness * (trial_curvature = t_curvature);
-    return SUANPAN_SUCCESS;
-}
-
-int Material::update_couple_trial_status(const vec& t_curvature, const vec&) { return update_couple_trial_status(t_curvature); }
-
-int Material::update_couple_trial_status(const vec& t_curvature, const vec&, const vec&) { return update_couple_trial_status(t_curvature); }
-
 int Material::clear_status() {
     if(!current_strain.is_empty()) current_strain.zeros();
     if(!current_strain_rate.is_empty()) current_strain_rate.zeros();
@@ -218,14 +166,6 @@ int Material::clear_status() {
     if(!initial_damping.is_empty()) trial_damping = current_damping = initial_damping;
     if(!initial_inertial.is_empty()) trial_inertial = current_inertial = initial_inertial;
 
-    if(!current_curvature.is_empty()) current_curvature.zeros();
-    if(!current_couple_stress.is_empty()) current_couple_stress.zeros();
-
-    if(!trial_curvature.is_empty()) trial_curvature.zeros();
-    if(!trial_couple_stress.is_empty()) trial_couple_stress.zeros();
-
-    if(!initial_couple_stiffness.is_empty()) trial_couple_stiffness = current_couple_stiffness = initial_couple_stiffness;
-
     return SUANPAN_SUCCESS;
 }
 
@@ -239,10 +179,6 @@ int Material::commit_status() {
     if(!trial_damping.is_empty()) current_damping = trial_damping;
     if(!trial_inertial.is_empty()) current_inertial = trial_inertial;
 
-    if(!trial_curvature.is_empty()) current_curvature = trial_curvature;
-    if(!trial_couple_stress.is_empty()) current_couple_stress = trial_couple_stress;
-    if(!trial_couple_stiffness.is_empty()) current_couple_stiffness = trial_couple_stiffness;
-
     return SUANPAN_SUCCESS;
 }
 
@@ -255,38 +191,6 @@ int Material::reset_status() {
     if(!trial_stiffness.is_empty()) trial_stiffness = current_stiffness;
     if(!trial_damping.is_empty()) trial_damping = current_damping;
     if(!trial_inertial.is_empty()) trial_inertial = current_inertial;
-
-    if(!trial_curvature.is_empty()) trial_curvature = current_curvature;
-    if(!trial_couple_stress.is_empty()) trial_couple_stress = current_couple_stress;
-    if(!trial_couple_stiffness.is_empty()) trial_couple_stiffness = current_couple_stiffness;
-
-    return SUANPAN_SUCCESS;
-}
-
-int Material::clear_couple_status() {
-    if(!current_curvature.is_empty()) current_curvature.zeros();
-    if(!current_couple_stress.is_empty()) current_couple_stress.zeros();
-
-    if(!trial_curvature.is_empty()) trial_curvature.zeros();
-    if(!trial_couple_stress.is_empty()) trial_couple_stress.zeros();
-
-    if(!initial_couple_stiffness.is_empty()) trial_couple_stiffness = current_couple_stiffness = initial_couple_stiffness;
-
-    return SUANPAN_SUCCESS;
-}
-
-int Material::commit_couple_status() {
-    if(!trial_curvature.is_empty()) current_curvature = trial_curvature;
-    if(!trial_couple_stress.is_empty()) current_couple_stress = trial_couple_stress;
-    if(!trial_couple_stiffness.is_empty()) current_couple_stiffness = trial_couple_stiffness;
-
-    return SUANPAN_SUCCESS;
-}
-
-int Material::reset_couple_status() {
-    if(!trial_curvature.is_empty()) trial_curvature = current_curvature;
-    if(!trial_couple_stress.is_empty()) trial_couple_stress = current_couple_stress;
-    if(!trial_couple_stiffness.is_empty()) trial_couple_stiffness = current_couple_stiffness;
 
     return SUANPAN_SUCCESS;
 }
@@ -317,12 +221,7 @@ void ConstantInertial(DataMaterial* M) {
     M->trial_inertial = mat(M->initial_inertial.memptr(), M->initial_inertial.n_rows, M->initial_inertial.n_cols, false, true);
 }
 
-void ConstantCoupleStiffness(DataCoupleMaterial* M) {
-    M->current_couple_stiffness = mat(M->initial_couple_stiffness.memptr(), M->initial_couple_stiffness.n_rows, M->initial_couple_stiffness.n_cols, false, true);
-    M->trial_couple_stiffness = mat(M->initial_couple_stiffness.memptr(), M->initial_couple_stiffness.n_rows, M->initial_couple_stiffness.n_cols, false, true);
-}
-
-void PureWrapper(Material* M) {
+void PureWrapper(DataMaterial* M) {
     M->current_strain.reset();
     M->current_strain_rate.reset();
     M->current_strain_acc.reset();
@@ -353,19 +252,6 @@ void PureWrapper(Material* M) {
     M->initial_inertial.reset();
     M->current_inertial.reset();
     M->trial_inertial.reset();
-
-    M->current_curvature.reset();
-    M->current_couple_stress.reset();
-
-    M->trial_curvature.reset();
-    M->trial_couple_stress.reset();
-
-    M->incre_curvature.reset();
-    M->incre_couple_stress.reset();
-
-    M->initial_couple_stiffness.reset();
-    M->current_couple_stiffness.reset();
-    M->trial_couple_stiffness.reset();
 }
 
 unique_ptr<Material> suanpan::unique_copy(const shared_ptr<Material>& P) { return nullptr == P ? nullptr : P->unique_copy(); }
