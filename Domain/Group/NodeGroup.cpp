389f74f76d49536aa7437d87dcf70ffb96e14da4
Theodore Chang <tlcfem@gmail.com>
2026-08-29 13:47:23 +0200

    Use parallel initialization

--- Domain/Group/NodeGroup.cpp
+++ Domain/Group/NodeGroup.cpp
@@ -44,51 +44,50 @@ void NodeGroup::initialize(const shared_ptr<DomainBase>& D) {
     // generate by direct assignment
     if(-1 == dof) return;
 
-    std::vector<uword> pond;
+    suanpan::set<uword> pond;
 
     if(-2 == dof)
         // generate by line segment between two points
-        for(auto& I : D->get_node_pool()) {
+        suanpan::for_all(D->get_node_pool(), [&](const shared_ptr<Node>& I) {
             auto& J = I->get_coordinate();
 
             const auto size = std::min(J.n_elem, std::min(s_node.n_elem, e_node.n_elem));
 
-            if(0 == size) continue;
+            if(0 == size) return;
 
             const vec s_point(s_node.mem, size), e_point(e_node.mem, size);
             const vec line_b = e_point - s_point;
 
             const auto dot_bb = dot(line_b, line_b);
-            if(std::sqrt(dot_bb) <= datum::eps) continue;
+            if(std::sqrt(dot_bb) <= datum::eps) return;
 
             const vec m_point(J.mem, size);
             const vec line_a = m_point - s_point;
 
             const auto dot_ab = dot(line_a, line_b);
 
-            if(const auto proj_ab = dot_ab / dot_bb; std::fabs(dot(line_a, line_a) - dot_ab * proj_ab) <= datum::eps * dot_bb && proj_ab >= 0. && proj_ab <= 1.) pond.emplace_back(I->get_tag());
-        }
+            if(const auto proj_ab = dot_ab / dot_bb; std::fabs(dot(line_a, line_a) - dot_ab * proj_ab) <= datum::eps * dot_bb && proj_ab >= 0. && proj_ab <= 1.) pond.insert(I->get_tag());
+        });
     else if(-3 == dof) {
         // generate by plane fitting
         const auto ref_error = norm(rule);
-        for(auto& I : D->get_node_pool()) {
+        suanpan::for_all(D->get_node_pool(), [&](const shared_ptr<Node>& I) {
             auto& J = I->get_coordinate();
 
             const auto size = std::min(J.n_elem, rule.n_elem - 1);
 
-            if(0 == size) continue;
+            if(0 == size) return;
 
-            if(const vec part(rule.mem, size), m_point(J.mem, size); std::fabs(dot(part, m_point) + rule.back()) <= datum::eps * ref_error) pond.emplace_back(I->get_tag());
-        }
+            if(const vec part(rule.mem, size), m_point(J.mem, size); std::fabs(dot(part, m_point) + rule.back()) <= datum::eps * ref_error) pond.insert(I->get_tag());
+        });
     }
     else {
         // generate by polynomial curve fitting
         const auto ref_error = norm(rule);
-        for(auto& I : D->get_node_pool())
-            if(auto& J = I->get_coordinate(); std::cmp_greater(J.n_elem, dof) && std::fabs(as_scalar(polyval(rule, vec{J(dof)}))) <= datum::eps * ref_error) pond.emplace_back(I->get_tag());
+        suanpan::for_all(D->get_node_pool(), [&](const shared_ptr<Node>& I) {
+            if(auto& J = I->get_coordinate(); std::cmp_greater(J.n_elem, dof) && std::fabs(as_scalar(polyval(rule, vec{J(dof)}))) <= datum::eps * ref_error) pond.insert(I->get_tag());
+        });
     }
 
-    suanpan_sort(pond.begin(), pond.end());
-
-    pool = pond;
+    pool = to_uvec(pond);
 }
