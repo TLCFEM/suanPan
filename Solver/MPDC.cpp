37f776ce036760bcd08c463e3f9cb63b6f034b5b
Theodore Chang <tlcfem@gmail.com>
2026-09-14 21:36:15 +0200

    Fix argument type in solve function call for incre_lambda calculation

--- Solver/MPDC.cpp
+++ Solver/MPDC.cpp
@@ -96,7 +96,7 @@ int MPDC::analyze() {
             disp_a -= aux_right * solve(aux_kernel, aux_border.t() * disp_a.head_rows(n_size));
         }
 
-        const vec incre_lambda = solve(mat(disp_a.rows(idx)), W->get_trial_settlement()(idx) - G->get_trial_displacement()(idx) - samurai.rows(idx));
+        const vec incre_lambda = solve(disp_a.rows(idx), W->get_trial_settlement()(idx) - G->get_trial_displacement()(idx) - samurai.rows(idx));
 
         samurai += disp_a * incre_lambda;
 
