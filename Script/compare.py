8d81b69ecd72209f06b24e334261cff127722b36
Theodore Chang <tlcfem@gmail.com>
2026-09-15 22:25:46 +0200

    Remove check for parallelization support message in skip function

--- Script/compare.py
+++ Script/compare.py
@@ -22,8 +22,6 @@ def skip(line: str):
         return True
     if " / __| | | | |_) / _` | '_ \\" in line:
         return True
-    if "This build lacks parallelization support." in line:
-        return True
     return "by tlc @" in line
 
 
